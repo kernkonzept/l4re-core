212bc6e489c09740ba4f521dbf078df434e9558d
wangyufen <wangyufen@huawei.com>
2014-11-10 00:00:00 +0000

    uclibc: add argument check in mknod
    mknod() in glibc/eglibc will check the argument, like this,
      ...
      if (k_dev != dev) {
          __set_errno (EINVAL);
          return -1;
       }
      ...
    So add argument check in uclibc's mknod() too.
    
    Change-Id: Ie3ce739e1d23fa16cc05364261268fe1599d91d3
    Signed-off-by: Wang Yufen <wangyufen@huawei.com>

--- uclibc/lib/contrib/uclibc/libc/sysdeps/linux/common/mknod.c
+++ uclibc/lib/contrib/uclibc/libc/sysdeps/linux/common/mknod.c
@@ -24,6 +24,10 @@ int mknod(const char *path, mode_t mode, dev_t dev)
 	/* We must convert the value to dev_t type used by the kernel.  */
 	k_dev = (dev) & ((1ULL << 32) - 1);
 
+	if (k_dev != dev) {
+		__set_errno (EINVAL);
+		return -1;
+	}
 	return INLINE_SYSCALL(mknod, 3, path, mode, (unsigned int)k_dev);
 }
 #endif
