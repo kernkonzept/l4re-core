05f3815f2ba8cfdffa77983633c0eed896935f03
Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>
2015-01-19 00:00:00 +0000

    uclibc/libc: ppc64 etc: Fix sync_file_range
    Fix copy'n paste error for ppc64 and other sync_file_range2 arches
    
    Change-Id: I08e15f4f1e878518cdc943da7c47804b71875eda
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- uclibc/lib/contrib/uclibc/libc/sysdeps/linux/common/sync_file_range.c
+++ uclibc/lib/contrib/uclibc/libc/sysdeps/linux/common/sync_file_range.c
@@ -23,10 +23,13 @@
 static int __NC(sync_file_range)(int fd, off64_t offset, off64_t nbytes, unsigned int flags)
 {
 #  if defined __powerpc__ && __WORDSIZE == 64
-	return INLINE_SYSCALL(sync_file_range, 4, fd, offset, nbytes, flags);
+	return INLINE_SYSCALL(sync_file_range, 4, fd, flags, offset, nbytes);
 #  elif defined __mips__ && _MIPS_SIM == _ABIO32
 	return INLINE_SYSCALL(sync_file_range, 7, fd, 0,
 			OFF64_HI_LO(offset), OFF64_HI_LO(nbytes), flags);
+#  elif defined __NR_sync_file_range2
+	return INLINE_SYSCALL(sync_file_range, 6, fd, flags
+			OFF64_HI_LO(offset), OFF64_HI_LO(nbytes));
 #  else
 	return INLINE_SYSCALL(sync_file_range, 6, fd,
 			OFF64_HI_LO(offset), OFF64_HI_LO(nbytes), flags);
