3cd8f12414ca6447b585283ae04211360284f9b9
Zhiqiang Zhang <zhangzhiqiang.zhang@huawei.com>
2015-03-16 00:00:00 +0000

    uclibc/malloc: checked_request2size failure deadlocks
    For some rarely cases(almost App bugs), calling malloc with
    a very largre size, checked_request2size check will fail,set
    ENOMEM, and return 0 to caller.
    
    But this will let __malloc_lock futex locked and owned by the
    caller. In multithread circumstance, other thread calling
    malloc/calloc will NOT succeed and get locked.
    
    Change-Id: Iab6d8f643062fc80962ebad290dd889a22ed246c
    Signed-off-by: Zhiqiang Zhang <zhangzhiqiang.zhang@huawei.com>
    Signed-off-by: Bernhard Reutner-Fischer <rep.dot.nop@gmail.com>

--- uclibc/lib/contrib/uclibc/libc/stdlib/malloc-standard/malloc.c
+++ uclibc/lib/contrib/uclibc/libc/stdlib/malloc-standard/malloc.c
@@ -830,8 +830,6 @@ void* malloc(size_t bytes)
     }
 #endif
 
-    __MALLOC_LOCK;
-    av = get_malloc_state();
     /*
        Convert request size to internal form by adding (sizeof(size_t)) bytes
        overhead plus possibly more to obtain necessary alignment and/or
@@ -842,6 +840,9 @@ void* malloc(size_t bytes)
        */
 
     checked_request2size(bytes, nb);
+
+    __MALLOC_LOCK;
+    av = get_malloc_state();
 
     /*
        Bypass search if no frees yet
