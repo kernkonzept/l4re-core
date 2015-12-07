101327b7e8ad368505f1366fd3819e25234213f5
Waldemar Brodkorb <wbx@uclibc-ng.org>
2015-12-07 00:00:00 +0000

    uclibc: sync with glibc, add some missing defines
    These are already used by some applications, so add it.
    
    Change-Id: Iaf8fda308f7212416149adb54a5b41f4cab071f0

--- uclibc/lib/contrib/uclibc/include/fcntl.h
+++ uclibc/lib/contrib/uclibc/include/fcntl.h
@@ -63,6 +63,11 @@ __BEGIN_DECLS
 # define AT_REMOVEDIR		0x200	/* Remove directory instead of
 					   unlinking file.  */
 # define AT_SYMLINK_FOLLOW	0x400	/* Follow symbolic links.  */
+# ifdef __USE_GNU
+#  define AT_NO_AUTOMOUNT       0x800   /* Suppress terminal automount
+                                           traversal.  */
+#  define AT_EMPTY_PATH         0x1000  /* Allow empty relative pathname.  */
+# endif
 # define AT_EACCESS		0x200	/* Test access permitted for
 					   effective IDs, not real IDs.  */
 #endif
