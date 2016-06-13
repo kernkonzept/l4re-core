061053e844f4364afff683d5a618a03be693a59b
Philipp Eppelt <philipp.eppelt@kernkonzept.com>
2016-06-13 00:00:00 +0000

    test moe: Out of memory behaviour
    With gcc 5.3.1 the emergency pool is allocated dynamically using malloc. Moe
    does not support malloc, hence the emergency pool is not allocated. This
    test case simulates an out of memory situation and triggers "MOE: is
    terminating with 127" error.
    
    Change-Id: I98ff3c3d0ae02bd5afc76074f335acadc877b90c

---
+++ test/moe/test_oom.cc
@@ -0,0 +1,53 @@
+/*
+ * Copyright (C) 2015 Kernkonzept GmbH.
+ * Author(s): Philipp Eppelt <philipp.eppelt@kernkonzept.com>
+ *
+ * This file is distributed under the terms of the GNU General Public
+ * License, version 2.  Please see the COPYING-GPL-2 file for details.
+ */
+
+/*
+ * Test out-of-memory behaviour of moe. (Emergency pool allocation.)
+ *
+ * GCC 4.9: moe does not crash and returns error.
+ * GCC 5.3: moe terminates with 127
+ */
+
+#include <l4/re/dataspace>
+#include <l4/re/error_helper>
+#include <l4/re/env>
+#include <l4/re/util/cap_alloc>
+
+#include <l4/atkins/l4_assert>
+#include <l4/atkins/tap/main>
+
+/**
+ * Acquire as much memory as possible to generate an out-of-memory state in
+ * moe. If moe replied with ENOMEM, free all dataspace capabilities and try
+ * again.
+ * Expectation: Moe did not crash.
+ */
+TEST(TestOom, OutOfMemory)
+{
+  constexpr int MAX = 100;
+  {
+    L4Re::Util::Auto_del_cap<L4Re::Dataspace>::Cap caparr[MAX];
+    int i = 0;
+    for (i = 0; i < MAX; ++i)
+      {
+        caparr[i] =
+          L4Re::chkcap(L4Re::Util::make_auto_del_cap<L4Re::Dataspace>());
+        long ret =
+          L4Re::Env::env()->mem_alloc()->alloc(-1, caparr[i].get(),
+                                               L4Re::Mem_alloc::Continuous);
+        if (ret == -L4_ENOMEM)
+          break;
+      }
+
+    ASSERT_LT(i, MAX) << "Out-of-memory situation not reached.";
+  }
+
+  auto cap = L4Re::chkcap(L4Re::Util::make_auto_del_cap<L4Re::Dataspace>());
+  ASSERT_L4OK(L4Re::Env::env()->mem_alloc()->alloc(-1, cap.get(),
+                                                   L4Re::Mem_alloc::Continuous));
+}
