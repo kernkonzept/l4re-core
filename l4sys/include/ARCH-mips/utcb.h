fda54968b2bedd2091b47b7104e05b77097ca949
Philipp Eppelt <philipp.eppelt@kernkonzept.com>
2016-10-31 00:00:00 +0000

    l4sys: MIPS exc_typeval func checks triggered exc bit
    l4_utcb_exc_typeval did not check for the exception number of the
    exception triggered via L4::Thread::ex_regs.
    This number is 0x101 and l4_utcb_exc_typeval checked only up to 0xff.
    
    Change-Id: I7655bf01b0771bbab41b00c6619395fe38a828ec

--- l4sys/include/ARCH-mips/utcb.h
+++ l4sys/include/ARCH-mips/utcb.h
@@ -111,7 +111,7 @@ L4_INLINE void l4_utcb_exc_pc_set(l4_exc_regs_t *u, l4_addr_t pc) L4_NOTHROW
 
 L4_INLINE l4_umword_t l4_utcb_exc_typeval(l4_exc_regs_t const *u) L4_NOTHROW
 {
-  return u->cause & 0xff;
+  return u->cause & 0x1ff;
 }
 
 L4_INLINE int l4_utcb_exc_is_pf(l4_exc_regs_t const *u) L4_NOTHROW
