a2a70e14cfa859d5a913e41c57ff45383d98b097
Thomas Petazzoni <thomas.petazzoni@free-electrons.com>
2016-12-05 00:00:00 +0000

    uclibc: sys/mman.h: remove madvise/posix_madvise prototypes on noMMU
    Change-Id: Ib5493b02f6a306a5d26fdf060124786089d2af28
    Signed-off-by: Thomas Petazzoni <thomas.petazzoni@free-electrons.com>

--- uclibc/lib/contrib/uclibc/include/sys/mman.h
+++ uclibc/lib/contrib/uclibc/include/sys/mman.h
@@ -99,6 +99,7 @@ static __inline__ int msync (void *__addr, size_t __len, int __flags) { return 0
 
 #endif
 
+#ifdef __ARCH_USE_MMU__
 #if defined __USE_BSD && (defined __UCLIBC_LINUX_SPECIFIC__ || defined __UCLIBC_HAS_THREADS_NATIVE__)
 /* Advise the system about particular usage patterns the program follows
    for the region starting at ADDR and extending LEN bytes.  */
@@ -108,6 +109,8 @@ extern int madvise (void *__addr, size_t __len, int __advice) __THROW;
 /* This is the POSIX name for this function.  */
 extern int posix_madvise (void *__addr, size_t __len, int __advice) __THROW;
 #endif
+#endif /* __ARCH_USE_MMU__ */
+
 
 #if defined __UCLIBC_HAS_REALTIME__
 # ifdef __ARCH_USE_MMU__
