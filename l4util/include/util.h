b541703be2d7af277fbadce0e148248eacd094cd
Philipp Eppelt <philipp.eppelt@kernkonzept.com>
2017-01-09 00:00:00 +0000

    l4util: doxygen brief removed
    Change-Id: I3ddaad1c30f8284a0069c65c3b827d2ea97a1498

--- l4util/include/util.h
+++ l4util/include/util.h
@@ -1,6 +1,5 @@
 /**
- * \file
- * \brief Utilities, generic file
+ * Utilities, generic file
  */
 /*
  * (c) 2008-2009 Adam Lackorzynski <adam@os.inf.tu-dresden.de>,
@@ -25,7 +24,7 @@
 EXTERN_C_BEGIN
 
 /**
- * \brief Calculate l4 timeouts
+ * Calculate l4 timeouts
  * \ingroup l4util_api
  * \param mus   time in microseconds. Special cases:
  *              - 0 - > timeout 0
@@ -35,29 +34,31 @@ EXTERN_C_BEGIN
 L4_CV l4_timeout_s l4util_micros2l4to(unsigned int mus) L4_NOTHROW;
 
 /**
- * \brief Suspend thread for a period of \a ms milliseconds
+ * Suspend thread for a period of \a ms milliseconds
  * \param ms Time in milliseconds
  * \ingroup l4util_api
  */
 L4_CV void l4_sleep(int ms) L4_NOTHROW;
 
-/* \brief Suspend thread for a period of \a us microseconds.
+/**
+ * Suspend thread for a period of \a us microseconds.
  * \param us Time in microseconds
  * \ingroup l4util_api
  *
  * WARNING: This function is mostly bogus since the timer resolution of
- *          current L4 implementations is about 1ms! */
+ *          current L4 implementations is about 1ms!
+ */
 L4_CV void l4_usleep(int us) L4_NOTHROW;
 
 /**
- * \brief Go sleep and never wake up.
+ * Go sleep and never wake up.
  * \ingroup l4util_api
  *
  */
 L4_INLINE void l4_sleep_forever(void) L4_NOTHROW L4_NORETURN;
 
 /**
- * \brief Touch data area to force mapping (read-only)
+ * Touch data area to force mapping (read-only)
  * \ingroup l4util_api
  *
  * \param addr Start of memory area to touch.
@@ -67,7 +68,7 @@ L4_INLINE void
 l4_touch_ro(const void *addr, unsigned size) L4_NOTHROW;
 
 /**
- * \brief Touch data areas to force mapping (read-write)
+ * Touch data areas to force mapping (read-write)
  * \ingroup l4util_api
  *
  * \param addr Start of memory area to touch.
