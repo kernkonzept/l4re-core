3bb06967cb94838d11b90a8c350b2a8c68f7c6ef
Alexander Warg <alexander.warg@kernkonzept.com>
2017-04-24 00:00:00 +0000

    moe: Support L4::Ipc_svr::Server_iface
    Add minimal support for The Server_iface class to prevent the nullptr
    dereference in the L4Re::Icu_svr helper, used by the Sched_proxy.
    
    Fixes ticket #512.
    
    Change-Id: I3a8e33cbd8d110197d4779510f8e74262bde277b

--- moe/server/src/obj_reg.h
+++ moe/server/src/obj_reg.h
@@ -38,6 +38,7 @@ class Cap_alloc;
 
 class Object_pool
 : public L4::Basic_registry,
+  public L4::Ipc_svr::Server_iface,
   private L4::Irqep_t<Object_pool>
 {
   friend struct L4::Irqep_t<Object_pool>;
@@ -46,6 +47,33 @@ public:
   explicit Object_pool(Cap_alloc *ca);
   Cap_alloc *cap_alloc() const { return _cap_alloc; }
   cxx::H_list_t<Moe::Server_object> life;
+  int alloc_buffer_demand(L4::Type_info::Demand const &demand) override
+  {
+    if (demand.caps > 1
+        || demand.ports != 0
+        || demand.mem != 0
+        || demand.flags != 0)
+      return -L4_EINVAL;
+
+    return 0;
+  }
+
+  L4::Cap<void> get_rcv_cap(int index) const override
+  {
+    if (index == 0)
+      return L4::Cap<void>(Rcv_cap << L4_CAP_SHIFT);
+    else
+      return L4::Cap<void>::Invalid;
+  }
+
+  int realloc_rcv_cap(int) override
+  { return -L4_ENOMEM; }
+
+  int add_timeout(L4::Ipc_svr::Timeout *, l4_kernel_clock_t) override
+  { return -L4_ENODEV; }
+
+  int remove_timeout(L4::Ipc_svr::Timeout *) override
+  { return -L4_ENODEV; }
 
 private:
   Cap_alloc *_cap_alloc;
@@ -95,6 +123,7 @@ public:
 
   L4::Cap<L4::Kobject> alloc(Moe::Server_object *_o)
   {
+    extern Object_pool object_pool;
     // make sure we register an Epiface ptr
     L4::Epiface *o = _o;
     L4::Cap<L4::Kobject> cap = _non_gc.alloc<L4::Kobject>();
@@ -108,7 +137,7 @@ public:
     l4_factory_create_gate(L4_BASE_FACTORY_CAP, cap.cap(),
                            L4_BASE_THREAD_CAP, id);
 
-    _o->set_obj_cap(cap);
+    _o->set_server(&object_pool, cap, true);
     return cap;
   }
 
@@ -135,7 +164,7 @@ inline Object_pool::Object_pool(Cap_alloc *ca) : _cap_alloc(ca)
   L4Re::chksys(L4Re::Env::env()->factory()->create(c));
   L4Re::chksys(c->attach(l4_umword_t(self),
                          L4::Cap<L4::Thread>(L4_BASE_THREAD_CAP)));
-  set_obj_cap(c);
+  set_server(this, c, true);
   L4Re::chksys(L4::Cap<L4::Thread>(L4_BASE_THREAD_CAP)->register_del_irq(c));
 }
 
