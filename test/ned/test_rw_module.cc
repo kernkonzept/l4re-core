fc93be2a4731ffef86260b70ee73896bfb6cc8f9
Steffen Liebergeld <steffen.liebergeld@kernkonzept.com>
2017-04-24 00:00:00 +0000

    moe: test rw-module
    Change-Id: I42876b61843ce4f2c61dfca12c7985b8a75c6759
    Requires-Change-Id: I976e710604248d89e96472f22279405f7cb2ae23

---
+++ test/ned/test_rw_module.cc
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2017 Kernkonzept GmbH.
+ * Author(s): Steffen Liebergeld <steffen.liebergeld@kernkonzept.com>
+ *
+ * This file is distributed under the terms of the GNU General Public
+ * License, version 2. Please see the COPYING-GPL-2 file for details.
+ */
+#include <l4/atkins/tap/main>
+#include <l4/atkins/l4_assert>
+#include <l4/re/env>
+#include <l4/re/namespace>
+#include <l4/re/dataspace>
+#include <l4/re/error_helper>
+#include <l4/re/util/cap_alloc>
+
+using L4Re::chksys;
+using L4Re::chkcap;
+using L4Re::Util::Auto_cap;
+
+auto *env = L4Re::Env::env();
+
+class Rw_module: public ::testing::Test
+{
+public:
+  void SetUp() override
+  {
+    test_ns = chkcap(env->get_cap<L4Re::Namespace>("testns"),
+                     "discover 'testns' namespace");
+  }
+
+  L4::Cap<L4Re::Namespace> test_ns;
+};
+
+TEST_F(Rw_module, WriteToModule)
+{
+  Auto_cap<L4Re::Dataspace>::Cap ds =
+    chkcap(L4Re::Util::cap_alloc.alloc<L4Re::Dataspace>(), "cap alloc");
+
+  ASSERT_L4OK(test_ns->query("rw_module", ds.get()))
+    << "find 'rw_module.cfg' in testns";
+
+  L4Re::Dataspace::Stats s;
+  ASSERT_L4OK(ds->info(&s))
+    << "ds info";
+
+  ASSERT_EQ(L4Re::Dataspace::Map_rw, (s.flags & L4Re::Dataspace::Map_rw))
+    << "ds writable";
+
+  L4Re::Rm::Auto_region<void *> base;
+  ASSERT_L4OK(env->rm()->attach(&base, s.size, L4Re::Rm::Search_addr,
+                                L4::Ipc::make_cap_rw(ds.get())))
+    << "attach ds to rm";
+
+  memset(base.get(), 1, s.size);
+}
