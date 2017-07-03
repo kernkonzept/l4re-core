987bbbfdc0b43bfc0531f162bff65761d34c39ce
Waldemar Brodkorb <wbx@uclibc-ng.org>
2017-07-03 00:00:00 +0000

    uclibc/udp.h: sync completely with GNU C library
    Change-Id: I9ee996599e9ec034b47a1f0a2b39423e5f358d3f

--- uclibc/lib/contrib/uclibc/include/netinet/udp.h
+++ uclibc/lib/contrib/uclibc/include/netinet/udp.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 1991-1993,1995-1997,2004,2009 Free Software Foundation, Inc.
+/* Copyright (C) 1991-2017 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -47,9 +47,8 @@
 #ifndef __NETINET_UDP_H
 #define __NETINET_UDP_H    1
 
-#include <features.h>
 #include <sys/types.h>
-
+#include <stdint.h>
 
 /* UDP header as specified by RFC 768, August 1980. */
 
@@ -78,11 +77,17 @@ struct udphdr
 #define UDP_CORK	1	/* Never send partially complete segments.  */
 #define UDP_ENCAP	100	/* Set the socket to accept
 				   encapsulated packets.  */
+#define UDP_NO_CHECK6_TX 101	/* Disable sending checksum for UDP
+				   over IPv6.  */
+#define UDP_NO_CHECK6_RX 102	/* Disable accepting checksum for UDP
+				   over IPv6.  */
 
 /* UDP encapsulation types */
 #define UDP_ENCAP_ESPINUDP_NON_IKE 1	/* draft-ietf-ipsec-nat-t-ike-00/01 */
 #define UDP_ENCAP_ESPINUDP	2	/* draft-ietf-ipsec-udp-encaps-06 */
 #define UDP_ENCAP_L2TPINUDP	3	/* rfc2661 */
+#define UDP_ENCAP_GTP0		4	/* GSM TS 09.60 */
+#define UDP_ENCAP_GTP1U		5	/* 3GPP TS 29.060 */
 
 #define SOL_UDP            17      /* sockopt level for UDP */
 
