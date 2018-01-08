442cbc3ec5e68f4886fe504dd9dbf890c3b92cc2
Adam Lackorzynski <adam@l4re.org>
2018-01-08 00:00:00 +0000

    libstdc++-v3-5: Update to current upstream
    Current upstream is r253576
    
    Change-Id: I9dea3b0f09470b55d1e65c05eaf90fd1cabf916e
    Requires-Change-Id: I028e76d541a07f25d990143094fbe812a8a60e50

--- libstdc++-headers/include-5/bits/c++config.h
+++ libstdc++-headers/include-5/bits/c++config.h
@@ -31,7 +31,7 @@
 #define _GLIBCXX_CXX_CONFIG_H 1
 
 // The current version of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20170514
+#define __GLIBCXX__ 20171010
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
