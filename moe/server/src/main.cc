8cb4c81f8d487f60393c6a2c7b279286d852ccd9
Phillip Raffeck <phillip.raffeck@kernkonzept.com>
2018-02-05 00:00:00 +0000

    moe: Avoid warning with no debugger.
    Avoid a warning like
    KERNEL: Warning: nothing mapped: (Obj_space) from [0xbcfe2b78/ffffffff]:
    0000000a size: 00000001 to [0xbcfe2b30/ffffffff]
    if fiasco is compiled without CONFIG_JDB.
    
    Change-Id: I40ec0cd2c679454b0a3ab80ad4e5c14020304dbf

--- moe/server/src/main.cc
+++ moe/server/src/main.cc
@@ -551,7 +551,8 @@ int main(int argc, char**argv)
         root_name_space()->register_obj("iommu", Entry::F_rw, L4_BASE_IOMMU_CAP);
       root_name_space()->register_obj("sigma0", Entry::F_trusted | Entry::F_rw, L4_BASE_PAGER_CAP);
       root_name_space()->register_obj("mem", Entry::F_trusted | Entry::F_rw, Allocator::root_allocator());
-      root_name_space()->register_obj("jdb", Entry::F_trusted | Entry::F_rw, L4_BASE_DEBUGGER_CAP);
+      if (L4::Cap<void>(L4_BASE_DEBUGGER_CAP).validate().label())
+        root_name_space()->register_obj("jdb", Entry::F_trusted | Entry::F_rw, L4_BASE_DEBUGGER_CAP);
 
       char *cmdline = my_cmdline();
 
