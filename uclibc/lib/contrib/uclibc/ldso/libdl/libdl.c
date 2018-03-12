8aaaffae6bd293096358939bfc88f0719cba3aa5
Waldemar Brodkorb <wbx@uclibc-ng.org>
2018-03-12 00:00:00 +0000

    uclibc/libdl: segfault in dlclose fix
    Revert "Revert "libdl: fix size parameter when unmap library in dlclose""
    
    And adds this fix for 64 Bit systems.
    http://lists.busybox.net/pipermail/uclibc/2014-December/048737.html
    
    Change-Id: Ib493dadff20d5e10f7a01b5c438df3c6774e2215

--- uclibc/lib/contrib/uclibc/ldso/libdl/libdl.c
+++ uclibc/lib/contrib/uclibc/ldso/libdl/libdl.c
@@ -807,7 +807,7 @@ static int do_dlclose(void *vhandle, int need_fini)
 	int (*dl_elf_fini) (void);
 	void (*dl_brk) (void);
 	struct dyn_elf *handle;
-	unsigned int end;
+	ElfW(Addr) end = 0, start = (ElfW(Addr))(~0ULL);
 	unsigned int i, j;
 	struct r_scope_elem *ls, *ls_next = NULL;
 	struct elf_resolve **handle_rlist;
@@ -884,6 +884,8 @@ static int do_dlclose(void *vhandle, int need_fini)
 					i < tpnt->n_phent; ppnt++, i++) {
 				if (ppnt->p_type != PT_LOAD)
 					continue;
+				if (ppnt->p_vaddr < start)
+					start = ppnt->p_vaddr;
 				if (end < ppnt->p_vaddr + ppnt->p_memsz)
 					end = ppnt->p_vaddr + ppnt->p_memsz;
 			}
@@ -990,7 +992,9 @@ static int do_dlclose(void *vhandle, int need_fini)
 			}
 #endif
 
-			DL_LIB_UNMAP (tpnt, end - tpnt->mapaddr);
+			end = (end + ADDR_ALIGN) & PAGE_ALIGN;
+			start = start & ~ADDR_ALIGN;
+			DL_LIB_UNMAP (tpnt, end - start);
 			/* Free elements in RTLD_LOCAL scope list */
 			for (runp = tpnt->rtld_local; runp; runp = tmp) {
 				tmp = runp->next;
