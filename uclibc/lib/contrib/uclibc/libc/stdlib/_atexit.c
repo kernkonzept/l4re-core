1c03255da5c3454be95fc27519382d678b289c76
Waldemar Brodkorb <wbx@uclibc-ng.org>
2018-04-09 00:00:00 +0000

    libdl: cleanup old inline changelog
    Change-Id: I80db7ab53d211bbb952fade9b954b22d5fc2763e

--- uclibc/lib/contrib/uclibc/libc/stdlib/_atexit.c
+++ uclibc/lib/contrib/uclibc/libc/stdlib/_atexit.c
@@ -3,40 +3,6 @@
  * under the GNU Library General Public License.
  */
 
-/*
- * Dec 2000          Manuel Novoa III
- *
- *   Made atexit handling conform to standards... i.e. no args.
- *   Removed on_exit since it did not match gnu libc definition.
- *   Combined atexit and __do_exit into one object file.
- *
- * Feb 2001          Manuel Novoa III
- *
- *   Reworked file after addition of __uClibc_main.
- *   Changed name of __do_exit to atexit_handler.
- *   Changed name of __cleanup to __uClibc_cleanup.
- *   Moved declaration of __uClibc_cleanup to __uClibc_main
- *      where it is initialized with (possibly weak alias)
- *      _stdio_term.
- *
- * Jul 2001          Steve Thayer
- *
- *   Added an on_exit implementation (that now matches gnu libc definition.)
- *   Pulled atexit_handler out of the atexit object since it is now required by
- *   on_exit as well.  Renamed it to __exit_handler.
- *   Fixed a problem where exit functions stop getting called if one of
- *   them calls exit().
- *   As a side effect of these changes, abort() no longer calls the exit
- *   functions (it now matches the gnu libc definition).
- *
- * August 2002    Erik Andersen
- *   Added locking so atexit and friends can be thread safe
- *
- * August 2005    Stephen Warren
- *   Added __cxa_atexit and __cxa_finalize support
- *
- */
-
 #include <features.h>
 #include <unistd.h>
 #include <stdlib.h>
