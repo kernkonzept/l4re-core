0548bb2c9f73ae900312b2a1d740fce62ac392e8
Waldemar Brodkorb <wbrodkorb@conet.de>
2018-05-28 00:00:00 +0000

    uclibc/riscv64: add upstream architecture files (unused for now)
    Change-Id: Icbe7649325a0ce48973e96a4b0bc856e7646144e

--- uclibc/lib/contrib/uclibc/libc/sysdeps/linux/common/rename.c
+++ uclibc/lib/contrib/uclibc/libc/sysdeps/linux/common/rename.c
@@ -16,6 +16,12 @@ int rename(const char *oldpath, const char *newpath)
 {
 	return renameat(AT_FDCWD, oldpath, AT_FDCWD, newpath);
 }
+#elif defined __NR_renameat2
+# include <fcntl.h>
+int rename(const char *oldpath, const char *newpath)
+{
+	_syscall2(int, renameat2, const char *, oldpath, const char *, newpath)
+}
 #else
 _syscall2(int, rename, const char *, oldpath, const char *, newpath)
 #endif
