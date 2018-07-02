2dae3b892f51508df251809154be9466126ae17b
Christophe Lyon <christophe.lyon@st.com>
2018-07-02 00:00:00 +0000

    uclibc/nptl: Clear TLS area for static binaries.
    busybox init checks it has pid 1, but getpid() returns another value
    when building busybox statically. This is because the corresponding
    area is not cleared when allocated (it is allocated with
    MAP_UNINITIALIZED, whose behavior depends on the Linux kernel's
    CONFIG_MMAP_ALLOW_UNINITIALIZED).
    
    This patch fixes the problem by explicitly clearing the memory area.
    
    	* libpthread/nptl/sysdeps/generic/libc-tls.c (__libc_tls_setup):
    	Clear tlsblock.
    
    Change-Id: Id7ad69f5664847692f6e865e5875f8e59449d968
    Signed-off-by: Mickaël Guêné <mickael.guene@st.com>
    Signed-off-by: Christophe Lyon <christophe.lyon@st.com>

--- uclibc/lib/contrib/uclibc/libpthread/nptl/sysdeps/generic/libc-tls.c
+++ uclibc/lib/contrib/uclibc/libpthread/nptl/sysdeps/generic/libc-tls.c
@@ -166,6 +166,7 @@ __libc_setup_tls (size_t tcbsize, size_t tcbalign)
   tcb_offset = roundup (tcbsize, align ?: 1);
   tlsblock = sbrk (tcb_offset + memsz + max_align
 		     + TLS_PRE_TCB_SIZE + GL(dl_tls_static_size));
+  memset(tlsblock, '\0', tcb_offset + memsz + max_align + TLS_PRE_TCB_SIZE + GL(dl_tls_static_size));
   tlsblock += TLS_PRE_TCB_SIZE;
 # else
   /* In case a model with a different layout for the TCB and DTV
