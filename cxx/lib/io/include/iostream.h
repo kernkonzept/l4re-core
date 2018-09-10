9969ff9419eced367beb96c15d14de63df31bb6a
Adam Lackorzynski <adam@l4re.org>
2018-09-10 00:00:00 +0000

    cxx: Remove deprecated headers
    Those headers have been deprecated years ago,
    finally remove them.
    
    Change-Id: Ia97bcce229ce9c0619620d54c4381bfcef17f087

--- cxx/lib/io/include/iostream.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#warning 'iostream.h' is deprecated use 'iostream' instead.
-#include <l4/cxx/iostream>
--- cxx/lib/io/include/l4iostream.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#warning 'l4iostream.h' is deprecated, use 'l4iostream' instead.
-#include <l4/cxx/l4iostream>
