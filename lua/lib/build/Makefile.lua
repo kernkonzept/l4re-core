cac3b7d186306e866b608965317698d2f1ee3229
Marcus Hähnel <marcus.haehnel@kernkonzept.com>
2019-02-04 00:00:00 +0000

    lua: Build and use dependency files
    Change-Id: I57f68346040cdd606fe5b6c94edf98887972f846

--- lua/lib/build/Makefile.lua
+++ lua/lib/build/Makefile.lua
@@ -5,7 +5,8 @@ vpath %.h $(SRC_DIR)/../contrib/src
 
 %.o: %.c $(GENERAL_D_LOC) $(SRC_DIR)/Makefile.lua
 	@echo " CC $@"
-	$(VERBOSE)$(CC) -c $(CPPFLAGS) $(CFLAGS) $<
+	$(VERBOSE)$(CC) -c -MD -MP -MF $(@D)/.$(@F).d $(CPPFLAGS) $(CFLAGS) $<
 
+-include $(wildcard .*.d)
 include $(SRC_DIR)/../contrib/src/Makefile
 MYCFLAGS=-I$(SRC_DIR)/../contrib/src -DLUA_USE_L4RE $(L4_DEFINES) $(CFLAGS_L4_GENERIC) $(L4_INCLUDES)
