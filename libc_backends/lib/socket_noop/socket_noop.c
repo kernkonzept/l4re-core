6c81928eb09460d609b8f2d7c0e601b0c1b9c3f4
Yann Le Du <yann.le.du@kernkonzept.com>
2019-07-01 00:00:00 +0000

    libc_backends: remove null check in socket noop gethostent_r()
    Uclibc code works with the assumption h_errnop is always set.
    
    Change-Id: I59768defd382132b8f9e4a246412f9edfc866179

--- libc_backends/lib/socket_noop/socket_noop.c
+++ libc_backends/lib/socket_noop/socket_noop.c
@@ -153,8 +153,8 @@ int W gethostent_r(struct hostent *ret, char *buf, size_t buflen,
 {
   printf("Unimplemented: %s(%p, %p, %zd, %p, %p)\n", __func__,
          ret, buf, buflen, result, h_errnop);
-  if (h_errnop)
-    *h_errnop = HOST_NOT_FOUND;
+  *h_errnop = HOST_NOT_FOUND;
+
   if (result)
     *result = NULL;
   return -1;
