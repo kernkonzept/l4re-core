8d9a23785ca9144eb4b23c5ed3caef1efe8dbaac
Sarah Hoffmann <sarah.hoffmann@kernkonzept.com>
2020-01-06 00:00:00 +0000

    ned: remove interactive mode
    The mode is no longer needed because the same can be achived by
    starting ned-prompt and connecting it to ned's command control
    interface.
    
    Change-Id: I619f93ca2182d520106c7278ed6f56b165de04f6

--- ned/server/src/lua.cc
+++ ned/server/src/lua.cc
@@ -22,11 +22,6 @@
 #include <unistd.h>
 #include <getopt.h>
 
-#ifdef USE_READLINE
-#include <readline/history.h>
-#include <readline/readline.h>
-#endif
-
 #include "lua_cap.h"
 #include "lua.h"
 
@@ -73,11 +68,9 @@ static const luaL_Reg libs[] =
   { NULL, NULL }
 };
 
-static char const *const options = "+ie:c:";
+static char const *const options = "+e:c:";
 static struct option const loptions[] =
   {
-    { "interactive", 0, NULL, 'i' },
-    { "noexit", 0, NULL, 1 },
     { "execute", 1, NULL, 'e' },
     { "cmdcap", 1, NULL, 'c' },
     { 0, 0, 0, 0 }
@@ -103,57 +96,6 @@ execute_lua_buf(lua_State *l, char const *buf, size_t sz, char const *name)
   lua_pop(l, lua_gettop(l));
 
   return 0;
-}
-
-static void
-run_interactive(lua_State *lua, bool noexit)
-{
-#ifndef USE_READLINE
-  (void)noexit;
-  (void)lua;
-  fprintf(stderr, "Ned: Interactive mode not compiled in.\n");
-#else
-  printf("Ned: Interactive mode.\n");
-  const char *cmd;
-  for (;;)
-    {
-      cmd = readline((char *)"Ned: ");
-
-      if (0)
-        printf("INPUT: %s\n", cmd);
-
-      if (!cmd)
-        {
-          if (noexit)
-            continue;
-          break;
-        }
-
-      if (luaL_loadbuffer(lua, cmd, strlen(cmd), "argument"))
-        {
-          fprintf(stderr, "lua couldn't parse '%s': %s.\n",
-                  cmd, lua_tostring(lua, -1));
-          lua_pop(lua, 1);
-        }
-      else
-        {
-          if (lua_pcall(lua, 0, 1, 0))
-            {
-              // errors during Factory::create() are returned as a table with
-              // { "msg" = message, "code" = return value }
-              // extract error message from table and push it on the stack
-              if (lua_istable(lua, -1))
-                lua_getfield(lua, -1, "msg");
-
-              fprintf(stderr, "lua couldn't execute '%s': %s.\n",
-                      cmd, lua_tostring(lua, -1));
-              lua_pop(lua, 1);
-            }
-          else
-            lua_pop(lua, lua_gettop(lua));
-        }
-    }
-#endif
 }
 
 class Command_dispatcher
@@ -255,12 +197,8 @@ int lua(int argc, char const *const *argv)
 {
   printf("Ned says: Hi World!\n");
 
-  enum Mode { None, Interactive, Cmd_channel };
+  enum Mode { None, Cmd_channel };
   Mode mode = None;
-  bool noexit = false;
-
-  if (argc < 2)
-    mode = Interactive;
 
   lua_State *L;
   L = luaL_newstate();
@@ -289,14 +227,6 @@ int lua(int argc, char const *const *argv)
     {
       switch (opt)
         {
-        case 'i':
-          if (mode == None)
-            mode = Interactive;
-          else
-            fprintf(stderr, "Must set either interactive or command channel mode."
-                            " Interactive switch ignored.\n");
-          break;
-        case 1: noexit = true; break;
         case 'e':
           {
             int err = execute_lua_buf(L, optarg, strlen(optarg), optarg);
@@ -305,21 +235,12 @@ int lua(int argc, char const *const *argv)
             break;
           }
         case 'c':
-          if (mode == Interactive)
-            fprintf(stderr, "Must set either interactive or command channel mode."
-                            " command channel capability ignored.\n");
-          else
-            {
-              cmd_client = optarg;
-              mode = Cmd_channel;
-            }
+          cmd_client = optarg;
+          mode = Cmd_channel;
           break;
         default: break;
         }
     }
-
-  if (optind >= argc && mode == None)
-    mode = Interactive;
 
   // everything following the first non-option argument is considered an
   // argument for the Lua script and added to the 'arg' table
@@ -358,9 +279,7 @@ int lua(int argc, char const *const *argv)
 
   lua_gc(L, LUA_GCCOLLECT, 0);
 
-  if (mode == Interactive)
-    run_interactive(L, noexit);
-  else if (mode == Cmd_channel)
+  if (mode == Cmd_channel)
     {
       L4Re::Util::Registry_server<L4Re::Util::Br_manager_timeout_hooks> server;
       Command_dispatcher cmd_dispatch(L);
