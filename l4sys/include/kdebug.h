5a56592f7194cce7d5c4c96b14035e53ae35937b
Frank Mehnert <frank.mehnert@kernkonzept.com>
2020-01-13 00:00:00 +0000

    l4sys/kdebug.h: use l4_bytes_to_mwords()
    Simplify the code using the new function l4_bytes_to_mwords().
    
    Change-Id: I91cc078f7e7392dfa347394d274b8759a5950b78

--- l4sys/include/kdebug.h
+++ l4sys/include/kdebug.h
@@ -76,9 +76,7 @@ __kdebug_text(unsigned op, char const *text, unsigned len) L4_NOTHROW
   __builtin_memcpy(&mr->mr[2], text, len);
   res = l4_ipc_call(L4_BASE_DEBUGGER_CAP, u,
                     l4_msgtag(L4_PROTO_DEBUGGER,
-                              (len + sizeof(l4_umword_t) -1)
-                              / sizeof(l4_umword_t) + 2,
-                              0, 0),
+                              l4_bytes_to_mwords(len) + 2, 0, 0),
                     L4_IPC_NEVER);
   __builtin_memcpy(mr, &store, sizeof(*mr));
   return res;
@@ -105,9 +103,7 @@ __kdebug_3_text(unsigned op, char const *text, unsigned len,
   __builtin_memcpy(&mr->mr[5], text, len);
   res = l4_ipc_call(L4_BASE_DEBUGGER_CAP, u,
                     l4_msgtag(L4_PROTO_DEBUGGER,
-                             (len + sizeof(l4_umword_t) -1)
-                             / sizeof(l4_umword_t) + 5,
-                             0, 0),
+                             l4_bytes_to_mwords(len) + 5, 0, 0),
                     L4_IPC_NEVER);
   __builtin_memcpy(mr, &store, sizeof(*mr));
   return res;
