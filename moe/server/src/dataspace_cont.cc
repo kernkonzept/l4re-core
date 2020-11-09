25b5302c2216f63f5266549a05eb81cb6e4ad852
Philipp Eppelt <philipp.eppelt@kernkonzept.com>
2020-11-09 00:00:00 +0000

    moe: use l4_task_unmap_batch in DS_cont::unmap
    The Dataspace_cont::unmap function unmaps each chunk with a separate
    syscall. This change uses l4_task_unmap_batch to combine unmap calls
    instead.
    
    Note, this also adds the missing UL to ~0 in l.84.
    
    Fixes #CD-348.
    
    Change-Id: I85c13db06a1623dc31a6c50897a736ea44955662

--- moe/server/src/dataspace_cont.cc
+++ moe/server/src/dataspace_cont.cc
@@ -86,14 +86,30 @@ void Moe::Dataspace_cont::unmap() const noexcept
 {
   unsigned long size = round_size();
   l4_addr_t offs = 0;
+  enum { Fpages_array_length = L4_UTCB_GENERIC_DATA_SIZE - 2 };
+  l4_fpage_t fpages[Fpages_array_length];
+  unsigned pages_idx = 0;
 
   while (size)
     {
-      Address addr = address(offs, L4Re::Dataspace::F::RWX, ~0);
-      l4_task_unmap(L4_BASE_TASK_CAP, addr.fp(), L4_FP_OTHER_SPACES);
+      Address addr = address(offs, L4Re::Dataspace::F::RWX, ~0UL);
+      fpages[pages_idx++] = addr.fp();
+
       size -= (1UL << l4_fpage_size(addr.fp()));
       offs  += (1UL << l4_fpage_size(addr.fp()));
+
+      // flush the fpages array, when it's full.
+      if (pages_idx == Fpages_array_length)
+        {
+          l4_task_unmap_batch(L4_BASE_TASK_CAP, fpages, pages_idx,
+                              L4_FP_OTHER_SPACES);
+          pages_idx = 0;
+        }
     }
+
+  if (pages_idx > 0)
+    l4_task_unmap_batch(L4_BASE_TASK_CAP, fpages, pages_idx,
+                        L4_FP_OTHER_SPACES);
 }
 
 int
