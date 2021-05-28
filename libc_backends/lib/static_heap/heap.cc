fff04994ad0ce9feae754dee305c0c620b8b64bc
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2021-05-28 15:55:16 +0200

    libc_backends: add backend for statically allocated heap
    Change-Id: Iaa5614cbcd647ac3fefbb28d24a9b4cfd3d95d83

---
+++ libc_backends/lib/static_heap/heap.cc
@@ -0,0 +1,41 @@
+#include <bits/l4-malloc.h>
+#include <errno.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+
+extern char __heap_start[];
+extern char __heap_end[];
+static char *heap_pos = __heap_start;
+
+void *uclibc_morecore(long bytes)
+{
+  if (bytes <= 0)
+    return heap_pos;
+
+  if (heap_pos + bytes > __heap_end)
+    return (void*)-1;
+
+  void *ret = heap_pos;
+  heap_pos += bytes;
+  return ret;
+}
+
+void *mmap(void * /*addr*/, size_t /*length*/, int /*prot*/, int /*flags*/,
+           int /*fd*/, off_t /*offset*/) __THROW
+{
+  errno = ENOSYS;
+  return MAP_FAILED;
+}
+
+int munmap(void * /*addr*/, size_t /*length*/) __THROW
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+void *mremap (void * /*addr*/, size_t /*old_len*/, size_t /*new_len*/,
+		     int /*flags*/, ...) __THROW
+{
+  errno = ENOSYS;
+  return MAP_FAILED;
+}
