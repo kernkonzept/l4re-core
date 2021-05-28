9ef5b76c4d6a1d02d3eaa1fd4e68024c236bb455
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2021-05-28 15:56:33 +0200

    minimal_io: cover more unix functions
    Change-Id: Ia8247c38c8b3d4a63ef179f5ef41750de157b34a

--- libc_backends/lib/minimal_io/be.cc
+++ libc_backends/lib/minimal_io/be.cc
@@ -10,9 +10,29 @@
  */
 
 #include <l4/re/env>
+#include <sys/ioctl.h>
 #include <sys/types.h>
 #include <unistd.h>
 #include <errno.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+int open(const char * /*name*/, int /*flags*/, ...)
+{
+  errno = ENOENT;
+  return -1;
+}
+
+int open64(const char * /*name*/, int /*flags*/, ...)
+{
+  errno = ENOENT;
+  return -1;
+}
+
+int close(int /*fd*/)
+{
+  return 0;
+}
 
 extern "C" ssize_t write(int fd, const void *buf, size_t count)
 {
@@ -41,5 +61,53 @@ extern "C" __off_t lseek(int, __off_t, int) L4_NOTHROW
 extern "C" __off64_t lseek64(int, __off64_t, int) L4_NOTHROW
 {
   errno = EBADF;
+  return -1;
+}
+
+int rename(const char * /*oldpath*/, const char * /*newpath*/) __THROW
+{
+  errno = ENOENT;
+  return -1;
+}
+
+int rmdir(const char * /*pathname*/) __THROW
+{
+  errno = ENOENT;
+  return -1;
+}
+
+int unlink(const char * /*pathname*/) __THROW
+{
+  errno = ENOENT;
+  return -1;
+}
+
+int stat(const char * /*pathname*/, struct stat * /*statbuf*/) __THROW
+{
+  errno = ENOENT;
+  return -1;
+}
+
+int fstat(int /*fd*/, struct stat * /*statbuf*/) __THROW
+{
+  errno = ENOSYS;
+  return -1;
+}
+
+int ioctl(int /*fd*/, unsigned long /*request*/, ...) __THROW
+{
+  errno = ENOTTY;
+  return -1;
+}
+
+int fcntl(int /*fd*/, int /*cmd*/, ...)
+{
+  errno = ENOTTY;
+  return -1;
+}
+
+int mkdir(const char * /*pathname*/, mode_t /*mode*/) __THROW
+{
+  errno = EROFS;
   return -1;
 }
