6e88f8844aae84ece408696b41240d3690f7058f
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2021-05-28 16:15:27 +0200

    moe: only include vesa support on x86 platforms
    Change-Id: I412a2167820c19e67b7eb7296409aa3206782ffb

--- moe/server/src/vesa_fb.h
+++ moe/server/src/vesa_fb.h
@@ -10,5 +10,9 @@
 
 #include <l4/util/l4mod.h>
 
+#if defined(ARCH_x86) || defined(ARCH_amd64)
 void init_vesa_fb(l4util_l4mod_info *mbi);
-
+#else
+static inline void init_vesa_fb(l4util_l4mod_info *)
+{}
+#endif
