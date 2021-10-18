09034e29446235070cbef261e4ebd7ec1bbd958e
Toni Dietze <toni.dietze@kernkonzept.com>
2021-10-18 00:00:00 +0000

    l4sys: clarify IPC gate documentation, esp. forwarding
    See ticket #CD-478 point 4.
    
    Change-Id: Ica100c800ab3c5f6973430414f3d73d0db558929

--- l4sys/include/ipc_gate.h
+++ l4sys/include/ipc_gate.h
@@ -1,20 +1,3 @@
-/**
- * \file
- * The C IPC gate interface.
- *
- * IPC gates are used to create secure communication channels between threads.
- * An IPC gate object can be created using the \ref l4_factory_api interface.
- * With l4_ipc_gate_bind_thread() a thread is bound to an IPC gate which then
- * receives all messages sent to that IPC gate.
- *
- * The l4_ipc_gate_bind_thread() call allows to assign each IPC gate a kernel
- * protected, machine-word sized payload called a *label*. It securely
- * identifies the gate. The lower two bits of the *label* can be used to encode
- * rights bits. The kernel combines these bits with the capability rights, so a
- * programmer usually should not pick the lower two bits for the *label*. The
- * *label* is only visible in the task which is running the thread the IPC gate
- * was bound to and cannot be altered by the sender.
- */
 /*
  * (c) 2009-2010 Adam Lackorzynski <adam@os.inf.tu-dresden.de>,
  *               Alexander Warg <warg@os.inf.tu-dresden.de>
@@ -33,6 +16,54 @@
  * invalidate any other reasons why the executable file might be covered by
  * the GNU General Public License.
  */
+
+/**
+ * \file
+ * The C IPC gate interface.
+ *
+ * IPC gates are used to create secure communication channels between protection
+ * domains. An IPC gate can be created using the \ref l4_factory_api interface.
+ *
+ * Depending on the permissions of the capability used, an IPC gate forwards IPC
+ * to the \ref l4_thread_api that is *bound* to the IPC gate (cf.
+ * l4_ipc_gate_bind_thread()). If the capability has the #L4_FPAGE_C_IPCGATE_SVR
+ * permission, only IPC using a protocol different from the #L4_PROTO_KOBJECT
+ * protocol is forwarded. Without the #L4_FPAGE_C_IPCGATE_SVR permission, all
+ * IPC is forwarded. The latter is the usual case for a client in a
+ * client/server scenario. When no thread is bound yet, the forwarded IPC blocks
+ * until a thread is bound or the IPC times out.
+ *
+ * Forwarded IPC is always forwarded to the userland of the bound thread. That
+ * means, the \ref l4_thread_api interface of the bound thread is not accessible
+ * via an IPC gate. The \ref l4_kernel_object_gate_api of an IPC gate is only
+ * accessible if the capability used has the #L4_FPAGE_C_IPCGATE_SVR permission
+ * (cf. previous paragraph). Conversely that means, if the capability used lacks
+ * the #L4_FPAGE_C_IPCGATE_SVR permission, \ref l4_kernel_object_gate_api calls
+ * are forwarded to the bound thread instead of being processed by the IPC gate
+ * itself. In a client/server scenario, a client should only get IPC gate
+ * capabilities without #L4_FPAGE_C_IPCGATE_SVR permission so the client cannot
+ * tamper with the IPC gate.
+ *
+ * When binding a thread to an IPC gate, a user-defined, kernel protected,
+ * machine-word sized payload called the IPC gate’s *label* is assigned to the
+ * IPC gate (cf. l4_ipc_gate_bind_thread()). When a send-only IPC or call IPC is
+ * forwarded via an IPC gate, the label provided by the sender is ignored and
+ * replaced by the IPC gate’s label where the two least significant bits are the
+ * result of bitwise disjunction of the corresponding label bits with the
+ * #L4_CAP_FPAGE_S and #L4_CAP_FPAGE_W permissions of the capability used.
+ * Hence, the label provided via l4_ipc_gate_bind_thread() should usually have
+ * its two least significant bits set to zero. The replaced label is only
+ * visible to the bound thread upon receive. However, the configured label of an
+ * IPC gate can also be queried via l4_ipc_gate_get_infos() if the capability
+ * used has the #L4_FPAGE_C_IPCGATE_SVR permission.
+ *
+ * \includefile{l4/sys/ipc_gate.h}
+ *
+ * For the C++ interface refer to the L4::Ipc_gate documentation.
+ *
+ * \see \ref l4_ipc_api
+ */
+
 #pragma once
 
 #include <l4/sys/utcb.h>
