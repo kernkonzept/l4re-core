e8aad9eefde0bd701d098312a03c929146815775
Georg Kotheimer <georg.kotheimer@kernkonzept.com>
2022-01-31 00:00:00 +0000

    uclibc: Improve suboptimal bzero macro definition
    The bzero macro definition in regex_old.c differs for no apparent reason
    from definitions of the bzero macro in other parts of the uclibc.
    
    bzero should have no return value, but in fact the suboptimal definition
    had one, which is not used and therefore led to the following warning:
    
    libc/misc/regex/regex_old.c:136:50: warning: right-hand operand of comma expression has no effect [-Wunused-value]
     #      define bzero(s, n)    (memset (s, '\0', n), (s))
                                  ~~~~~~~~~~~~~~~~~~~~^~~~~~
    
    Change-Id: I8259edde15ed5aebb3dfe1fe5a74c62ce24c211b

--- uclibc/lib/contrib/uclibc/libc/misc/regex/regex_old.c
+++ uclibc/lib/contrib/uclibc/libc/misc/regex/regex_old.c
@@ -133,7 +133,7 @@ char *realloc ();
 #    include <string.h>
 #    ifndef bzero
 #     ifndef _LIBC
-#      define bzero(s, n)	(memset (s, '\0', n), (s))
+#      define bzero(s, n)	(memset((s), '\0', (n)), (void) 0)
 #     else
 #      define bzero(s, n)	__bzero (s, n)
 #     endif
