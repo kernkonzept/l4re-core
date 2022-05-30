53e429444fc868d509c58a6ae02942c7c650aca6
Adam Lackorzynski <adam@l4re.org>
2022-05-30 00:00:00 +0000

    libc_backends/misc: Add some more sched funcs
    Change-Id: I45e02f66e00485a70ab6332b4ffbf74c1c31d34d

--- libc_backends/lib/misc/sched.c
+++ libc_backends/lib/misc/sched.c
@@ -6,6 +6,8 @@
  * Please see the COPYING-LGPL-2.1 file for details.
  */
 #include <sched.h>
+#include <errno.h>
+#include <unistd.h>
 
 int sched_get_priority_max(int policy)
 {
@@ -17,4 +19,25 @@ int sched_get_priority_min(int policy)
 {
   (void)policy;
   return 1;
+}
+
+int sched_getcpu(void)
+{
+  // In a general sense we don't know, so just report the first CPU
+  return 0;
+}
+
+int sched_getaffinity(pid_t pid, size_t cpusetsize,
+                      cpu_set_t *mask)
+{
+  (void)pid;
+  __CPU_SET_S(1, cpusetsize, mask);
+  return 0;
+}
+
+int nice(int inc)
+{
+  (void)inc;
+  errno = EPERM;
+  return -1;
 }
