c3ab5eb1510066047f56ca0f3ff496ce961cfccc
Frank Mehnert <frank.mehnert@kernkonzept.com>
2022-08-08 00:00:00 +0000

    l4sys: Doxygen fixes for inhibitor
    The link from the C interface to an enum defined in the C++ interface
    needs a proper class name. Also fix some other minor cosmetic issues
    adapting the documentation of the C interface to the C++ interface.
    
    See ticket #CD-817.
    
    Change-Id: If5e118f49ef588737908bd4dd65a2dabb56ec95d

--- l4re_c/include/inhibitor.h
+++ l4re_c/include/inhibitor.h
@@ -23,8 +23,8 @@ EXTERN_C_BEGIN
  * \param id      ID of the inhibitor lock that shall be acquired.
  * \param reason  Reason why the inhibitor lock is acquired.
  *                (Used for informing the user or debugging.)
- * \return 0 for success, <0 on error
- * \see L4Re::Inhibitor::acquire()
+ * \return 0 for success, <0 on error.
+ * \see L4Re::Inhibitor::acquire().
  */
 L4_CV long L4_EXPORT
 l4re_inhibitor_acquire(l4_cap_idx_t cap, l4_umword_t id,
@@ -34,24 +34,26 @@ l4re_inhibitor_acquire(l4_cap_idx_t cap, l4_umword_t id,
  * \brief Release an inhibitor lock.
  * \param cap  Capability for the Inhibitor object (see L4Re::Inhibitor).
  * \param id   ID of inhibitor that shall be released.
- * \return 0 for success, <0 on error
- * \see L4Re::Inhibitor::release()
+ * \return 0 for success, <0 on error.
+ * \see L4Re::Inhibitor::release().
  */
 L4_CV long L4_EXPORT
 l4re_inhibitor_release(l4_cap_idx_t cap, l4_umword_t id);
 
 /**
  * \brief Get information for the next available inhibitor lock.
- * \param cap  Capability to the Inhibitor object (see L4Re::Inhibitor)
- * \param name a pointer to a buffer for the name of the lock.
- * \param len  the length of the available buffer (usually #Name_max is used)
- * \param current_id the ID of the last available lock, use -1 to get the
- *                   first lock.
- * \return the ID of the next available lock if there is one (in this case
- *         name shall contain the name of the inhibitor lock).
- * \return -L4_ENODEV if there are no more locks
- * \return any other negative failure value.
- * \see L4Re::Inhibitor::next_lock_info()
+ *
+ * \param cap         Capability to the Inhibitor object (see L4Re::Inhibitor)
+ * \param name        A pointer to a buffer for the name of the lock.
+ * \param len         The length of the available buffer (usually
+ *                    L4Re::Inhibitor::Name_max is used).
+ * \param current_id  The ID of the last available lock, use -1 to get the
+ *                    first lock.
+ * \retval >0: The ID of the next available lock if there is one (in this case
+ *             name shall contain the name of the inhibitor lock).
+ * \retval -L4_ENODEV if there are no more locks.
+ * \retval <0: Any other negative failure value.
+ * \see L4Re::Inhibitor::next_lock_info().
  */
 L4_CV long L4_EXPORT
 l4re_inhibitor_next_lock_info(l4_cap_idx_t cap, char *name,
