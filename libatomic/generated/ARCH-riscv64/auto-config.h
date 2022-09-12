5b172abe4b5072dd1d0e86cd9677d5afd37684ec
Georg Kotheimer <georg.kotheimer@kernkonzept.com>
2022-09-12 00:00:00 +0000

    riscv: Add libatomic from GCC 12.2.0
    Change-Id: I3ce64bd9e34f34039d3692e0eac2b8d926ffad1c

---
+++ libatomic/generated/ARCH-riscv64/auto-config.h
@@ -0,0 +1,299 @@
+/* auto-config.h.  Generated from auto-config.h.in by configure.  */
+/* auto-config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* Define if building universal (internal helper macro) */
+/* #undef AC_APPLE_UNIVERSAL_BUILD */
+
+/* Have __atomic_compare_exchange for 1 byte integers. */
+#define HAVE_ATOMIC_CAS_1 0 
+
+/* Have __atomic_compare_exchange for 16 byte integers. */
+#define HAVE_ATOMIC_CAS_16 0 
+
+/* Have __atomic_compare_exchange for 2 byte integers. */
+#define HAVE_ATOMIC_CAS_2 0 
+
+/* Have __atomic_compare_exchange for 4 byte integers. */
+#define HAVE_ATOMIC_CAS_4 1  
+
+/* Have __atomic_compare_exchange for 8 byte integers. */
+#define HAVE_ATOMIC_CAS_8 1  
+
+/* Have __atomic_exchange for 1 byte integers. */
+#define HAVE_ATOMIC_EXCHANGE_1 0 
+
+/* Have __atomic_exchange for 16 byte integers. */
+#define HAVE_ATOMIC_EXCHANGE_16 0 
+
+/* Have __atomic_exchange for 2 byte integers. */
+#define HAVE_ATOMIC_EXCHANGE_2 0 
+
+/* Have __atomic_exchange for 4 byte integers. */
+#define HAVE_ATOMIC_EXCHANGE_4 1  
+
+/* Have __atomic_exchange for 8 byte integers. */
+#define HAVE_ATOMIC_EXCHANGE_8 1  
+
+/* Have __atomic_fetch_add for 1 byte integers. */
+#define HAVE_ATOMIC_FETCH_ADD_1 0 
+
+/* Have __atomic_fetch_add for 16 byte integers. */
+#define HAVE_ATOMIC_FETCH_ADD_16 0 
+
+/* Have __atomic_fetch_add for 2 byte integers. */
+#define HAVE_ATOMIC_FETCH_ADD_2 0 
+
+/* Have __atomic_fetch_add for 4 byte integers. */
+#define HAVE_ATOMIC_FETCH_ADD_4 1  
+
+/* Have __atomic_fetch_add for 8 byte integers. */
+#define HAVE_ATOMIC_FETCH_ADD_8 1  
+
+/* Have __atomic_fetch_op for all op for 1 byte integers. */
+#define HAVE_ATOMIC_FETCH_OP_1 0 
+
+/* Have __atomic_fetch_op for all op for 16 byte integers. */
+#define HAVE_ATOMIC_FETCH_OP_16 0 
+
+/* Have __atomic_fetch_op for all op for 2 byte integers. */
+#define HAVE_ATOMIC_FETCH_OP_2 0 
+
+/* Have __atomic_fetch_op for all op for 4 byte integers. */
+#define HAVE_ATOMIC_FETCH_OP_4 1  
+
+/* Have __atomic_fetch_op for all op for 8 byte integers. */
+#define HAVE_ATOMIC_FETCH_OP_8 1  
+
+/* Have __atomic_load/store for 1 byte integers. */
+#define HAVE_ATOMIC_LDST_1 1  
+
+/* Have __atomic_load/store for 16 byte integers. */
+#define HAVE_ATOMIC_LDST_16 0 
+
+/* Have __atomic_load/store for 2 byte integers. */
+#define HAVE_ATOMIC_LDST_2 1  
+
+/* Have __atomic_load/store for 4 byte integers. */
+#define HAVE_ATOMIC_LDST_4 1  
+
+/* Have __atomic_load/store for 8 byte integers. */
+#define HAVE_ATOMIC_LDST_8 1  
+
+/* Have __atomic_test_and_set for 1 byte integers. */
+#define HAVE_ATOMIC_TAS_1 1  
+
+/* Have __atomic_test_and_set for 16 byte integers. */
+#define HAVE_ATOMIC_TAS_16 1  
+
+/* Have __atomic_test_and_set for 2 byte integers. */
+#define HAVE_ATOMIC_TAS_2 1  
+
+/* Have __atomic_test_and_set for 4 byte integers. */
+#define HAVE_ATOMIC_TAS_4 1  
+
+/* Have __atomic_test_and_set for 8 byte integers. */
+#define HAVE_ATOMIC_TAS_8 1  
+
+/* Define to 1 if the target supports __attribute__((alias(...))). */
+#define HAVE_ATTRIBUTE_ALIAS 1
+
+/* Define to 1 if the target supports __attribute__((dllexport)). */
+/* #undef HAVE_ATTRIBUTE_DLLEXPORT */
+
+/* Define to 1 if the target supports __attribute__((visibility(...))). */
+#define HAVE_ATTRIBUTE_VISIBILITY 1
+
+/* Define to 1 if you have the <dlfcn.h> header file. */
+#define HAVE_DLFCN_H 1
+
+/* Define to 1 if you have the <fenv.h> header file. */
+#define HAVE_FENV_H 1
+
+/* Define to 1 if the target supports __attribute__((ifunc(...))). */
+/* #undef HAVE_IFUNC */
+
+/* Have support for 1 byte integers. */
+#define HAVE_INT1 1  
+
+/* Have support for 16 byte integers. */
+#define HAVE_INT16 1  
+
+/* Have support for 2 byte integers. */
+#define HAVE_INT2 1  
+
+/* Have support for 4 byte integers. */
+#define HAVE_INT4 1  
+
+/* Have support for 8 byte integers. */
+#define HAVE_INT8 1  
+
+/* Define to 1 if you have the <inttypes.h> header file. */
+#define HAVE_INTTYPES_H 1
+
+/* Define to 1 if you have the <memory.h> header file. */
+#define HAVE_MEMORY_H 1
+
+/* Define to 1 if you have the <stdint.h> header file. */
+#define HAVE_STDINT_H 1
+
+/* Define to 1 if you have the <stdlib.h> header file. */
+#define HAVE_STDLIB_H 1
+
+/* Define to 1 if you have the <strings.h> header file. */
+#define HAVE_STRINGS_H 1
+
+/* Define to 1 if you have the <string.h> header file. */
+#define HAVE_STRING_H 1
+
+/* Define to 1 if you have the <sys/stat.h> header file. */
+#define HAVE_SYS_STAT_H 1
+
+/* Define to 1 if you have the <sys/types.h> header file. */
+#define HAVE_SYS_TYPES_H 1
+
+/* Define to 1 if you have the <unistd.h> header file. */
+#define HAVE_UNISTD_H 1
+
+/* Define ifunc resolver function argument. */
+#define IFUNC_RESOLVER_ARGS void
+
+/* Define to 1 if GNU symbol versioning is used for libatomic. */
+#define LIBAT_GNU_SYMBOL_VERSIONING 1
+
+/* Define to the sub-directory in which libtool stores uninstalled libraries.
+   */
+#define LT_OBJDIR ".libs/"
+
+/* Name of package */
+#define PACKAGE "libatomic"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT ""
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "GNU Atomic Library"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "GNU Atomic Library 1.0"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "libatomic"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "http://www.gnu.org/software/libatomic/"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0"
+
+/* The size of `char', as computed by sizeof. */
+/* #undef SIZEOF_CHAR */
+
+/* The size of `int', as computed by sizeof. */
+/* #undef SIZEOF_INT */
+
+/* The size of `long', as computed by sizeof. */
+/* #undef SIZEOF_LONG */
+
+/* The size of `short', as computed by sizeof. */
+/* #undef SIZEOF_SHORT */
+
+/* The size of `void *', as computed by sizeof. */
+/* #undef SIZEOF_VOID_P */
+
+/* Define to 1 if you have the ANSI C header files. */
+#define STDC_HEADERS 1
+
+/* Define if you can safely include both <string.h> and <strings.h>. */
+#define STRING_WITH_STRINGS 1
+
+/* Version number of package */
+#define VERSION "1.0"
+
+/* The word size in bytes of the machine. */
+#define WORDSIZE 8
+
+/* Define WORDS_BIGENDIAN to 1 if your processor stores words with the most
+   significant byte first (like Motorola and SPARC, unlike Intel). */
+#if defined AC_APPLE_UNIVERSAL_BUILD
+# if defined __BIG_ENDIAN__
+#  define WORDS_BIGENDIAN 1
+# endif
+#else
+# ifndef WORDS_BIGENDIAN
+/* #  undef WORDS_BIGENDIAN */
+# endif
+#endif
+
+#define MAYBE_HAVE_ATOMIC_LDST_1 HAVE_ATOMIC_LDST_1
+
+#define FAST_ATOMIC_LDST_16 HAVE_ATOMIC_LDST_16
+
+#define MAYBE_HAVE_ATOMIC_TAS_1 HAVE_ATOMIC_TAS_1
+
+#define MAYBE_HAVE_ATOMIC_TAS_2 HAVE_ATOMIC_TAS_2
+
+#define MAYBE_HAVE_ATOMIC_TAS_4 HAVE_ATOMIC_TAS_4
+
+#define MAYBE_HAVE_ATOMIC_TAS_8 HAVE_ATOMIC_TAS_8
+
+#define MAYBE_HAVE_ATOMIC_TAS_16 HAVE_ATOMIC_TAS_16
+
+#define MAYBE_HAVE_ATOMIC_EXCHANGE_1 HAVE_ATOMIC_EXCHANGE_1
+
+#define MAYBE_HAVE_ATOMIC_EXCHANGE_2 HAVE_ATOMIC_EXCHANGE_2
+
+#define MAYBE_HAVE_ATOMIC_EXCHANGE_4 HAVE_ATOMIC_EXCHANGE_4
+
+#define MAYBE_HAVE_ATOMIC_EXCHANGE_8 HAVE_ATOMIC_EXCHANGE_8
+
+#define FAST_ATOMIC_LDST_1 HAVE_ATOMIC_LDST_1
+
+#define MAYBE_HAVE_ATOMIC_EXCHANGE_16 HAVE_ATOMIC_EXCHANGE_16
+
+#define MAYBE_HAVE_ATOMIC_CAS_1 HAVE_ATOMIC_CAS_1
+
+#define MAYBE_HAVE_ATOMIC_CAS_2 HAVE_ATOMIC_CAS_2
+
+#define MAYBE_HAVE_ATOMIC_CAS_4 HAVE_ATOMIC_CAS_4
+
+#define MAYBE_HAVE_ATOMIC_CAS_8 HAVE_ATOMIC_CAS_8
+
+#define MAYBE_HAVE_ATOMIC_CAS_16 HAVE_ATOMIC_CAS_16
+
+#define MAYBE_HAVE_ATOMIC_FETCH_ADD_1 HAVE_ATOMIC_FETCH_ADD_1
+
+#define MAYBE_HAVE_ATOMIC_FETCH_ADD_2 HAVE_ATOMIC_FETCH_ADD_2
+
+#define MAYBE_HAVE_ATOMIC_FETCH_ADD_4 HAVE_ATOMIC_FETCH_ADD_4
+
+#define MAYBE_HAVE_ATOMIC_FETCH_ADD_8 HAVE_ATOMIC_FETCH_ADD_8
+
+#define MAYBE_HAVE_ATOMIC_LDST_2 HAVE_ATOMIC_LDST_2
+
+#define MAYBE_HAVE_ATOMIC_FETCH_ADD_16 HAVE_ATOMIC_FETCH_ADD_16
+
+#define MAYBE_HAVE_ATOMIC_FETCH_OP_1 HAVE_ATOMIC_FETCH_OP_1
+
+#define MAYBE_HAVE_ATOMIC_FETCH_OP_2 HAVE_ATOMIC_FETCH_OP_2
+
+#define MAYBE_HAVE_ATOMIC_FETCH_OP_4 HAVE_ATOMIC_FETCH_OP_4
+
+#define MAYBE_HAVE_ATOMIC_FETCH_OP_8 HAVE_ATOMIC_FETCH_OP_8
+
+#define MAYBE_HAVE_ATOMIC_FETCH_OP_16 HAVE_ATOMIC_FETCH_OP_16
+
+#ifndef WORDS_BIGENDIAN
+#define WORDS_BIGENDIAN 0
+#endif
+
+#define FAST_ATOMIC_LDST_2 HAVE_ATOMIC_LDST_2
+
+#define MAYBE_HAVE_ATOMIC_LDST_4 HAVE_ATOMIC_LDST_4
+
+#define FAST_ATOMIC_LDST_4 HAVE_ATOMIC_LDST_4
+
+#define MAYBE_HAVE_ATOMIC_LDST_8 HAVE_ATOMIC_LDST_8
+
+#define FAST_ATOMIC_LDST_8 HAVE_ATOMIC_LDST_8
+
+#define MAYBE_HAVE_ATOMIC_LDST_16 HAVE_ATOMIC_LDST_16
