7255cb8a5a2751349027cb8ef0b74f4b7b431c4f
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2023-01-09 00:00:00 +0000

    ldso: stub __register_frame_info
    When compiling libgcc with an l4re-gcc calls to __register_frame_info
    are emitted. This is caused implicitly by our uclibc-derived libc which
    triggers different behaviour in libgcc than the Linux glibc case.
    
    Change-Id: Ida6d4bb43997f05b7762e8faec4a22c14fec60b0

--- ldso/ldso/fixup.c
+++ ldso/ldso/fixup.c
@@ -7,12 +7,16 @@ void __cxa_finalize(void);
 void _Jv_RegisterClasses(void);
 int __aeabi_unwind_cpp_pr0(void);
 int __aeabi_unwind_cpp_pr1(void);
+void __deregister_frame_info(void);
+void __register_frame_info(void);
 void __deregister_frame_info_bases(void);
 void __register_frame_info_bases(void);
 enum { _URC_FAILURE  = 9 };
 
 void __cxa_finalize(void) {}
 void _Jv_RegisterClasses(void) {}
+void __deregister_frame_info(void) {}
+void __register_frame_info(void) {}
 void __deregister_frame_info_bases(void) {}
 void __register_frame_info_bases(void) {}
 
