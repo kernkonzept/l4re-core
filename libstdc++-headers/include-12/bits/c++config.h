59839bee2d38056c73ca4d175498c7da198098f9
Adam Lackorzynski <adam@l4re.org>
2023-01-16 00:00:00 +0000

    libstdc++-v3: Update gcc-12 variant
    Taken from 6cc80436a9359e08401930086f309702184779a0
    
    Change-Id: Ie9d3a9f12e40bafa5adc9a6f940f1bae1a872b40

--- libstdc++-headers/include-12/bits/c++config.h
+++ libstdc++-headers/include-12/bits/c++config.h
@@ -34,7 +34,7 @@
 #define _GLIBCXX_RELEASE 12
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20221014
+#define __GLIBCXX__ 20230122
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
