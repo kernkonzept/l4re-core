b6e408ebd03bb2f535b9133778469fee273f9910
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2023-03-28 08:32:20 +0200

    Remove License-Ref-kk-custom
    Change-Id: I91005c83c2891bf0852130c27af1e2cfea53fdce

--- ned/ned-prompt/src/ned-prompt.cc
+++ ned/ned-prompt/src/ned-prompt.cc
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-2.0-only or License-Ref-kk-custom
+// SPDX-License-Identifier: GPL-2.0-only 
 /*
  * Copyright (C) 2020 Kernkonzept GmbH.
  * Author(s): Adam Lackorzynski <adam.lackorzynski@kernkonzept.com>
