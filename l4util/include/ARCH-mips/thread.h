cbe2f4fcb4897eba79fb47b68e6d99405be6690c
Frank Mehnert <frank.mehnert@kernkonzept.com>
2023-05-08 00:00:00 +0000

    l4util: MIPS: Redo thread helper
    Recent tool chains reveal that globally specified assembler code is not
    the right approach. Therefore, place the code in the helper function.
    
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=884821
    
    Change-Id: Iaf21ed32ad8dfd181d3fd2bf90c282aeff61a1e9

--- l4util/include/ARCH-mips/thread.h
+++ l4util/include/ARCH-mips/thread.h
@@ -10,66 +10,47 @@
 #include <l4/sys/compiler.h>
 
 #if _MIPS_SZPTR == 32
-#define L4UTIL_THREAD_START_LOAD_FUNC_ADDR(func) "  la $t9," func "\n"
+# define L4UTIL_THREAD_START_LOAD_FUNC_ADDR(func) "  la $t9," func "\n"
 #else
-#define L4UTIL_THREAD_START_LOAD_FUNC_ADDR(func) "  dla $t9," func "\n"
+# define L4UTIL_THREAD_START_LOAD_FUNC_ADDR(func) "  dla $t9," func "\n"
 #endif
 
 #if _MIPS_SIM == _ABIO32
-#define L4UTIL_THREAD_START_SETUP_GP \
+# define L4UTIL_THREAD_START_SETUP_GP \
 "  bal 10f \n" \
 "   nop    \n" \
 "10:       \n" \
 "  .cpload $31 \n"
 #else
-#define L4UTIL_THREAD_START_SETUP_GP \
+# define L4UTIL_THREAD_START_SETUP_GP \
 " bal 10f \n" \
 "  nop    \n" \
 "10:      \n" \
 "  .cpsetup $31, $25, 10b \n"
 #endif
 
-
-#define __L4UTIL_THREAD_FUNC(name) \
+#define L4UTIL_THREAD_FUNC_MIPS_TEMPLATE(name, locality) \
 EXTERN_C_BEGIN \
-static void  __attribute__((used)) name##_worker_function(void); \
-asm ("\n" \
-  "  .type " #name ", function \n" \
-  "  .global " #name " \n" \
-  #name ": \n" \
-  "  .set push \n" \
-  "  .set noreorder \n" \
-  L4UTIL_THREAD_START_SETUP_GP \
-  L4UTIL_THREAD_START_LOAD_FUNC_ADDR(#name "_worker_function") \
-  "  jal $t9 \n" \
-  "   nop    \n"\
-  "  .set pop" \
-); \
+locality L4_NORETURN void name(void) \
+{ \
+  asm("  .set push \n" \
+      "  .set noreorder \n" \
+      L4UTIL_THREAD_START_SETUP_GP \
+      L4UTIL_THREAD_START_LOAD_FUNC_ADDR(#name "_mips_helper_func") \
+      "  jal $t9 \n" \
+      "   nop    \n"\
+      "  .set pop" \
+  ); \
+  __builtin_unreachable(); \
+} \
+static L4_NORETURN void __attribute__((used)) name ##_mips_helper_func(void); \
 EXTERN_C_END \
-static L4_NORETURN void name##_worker_function(void)
+static L4_NORETURN void name##_mips_helper_func(void)
 
-#define L4UTIL_THREAD_FUNC(name) __L4UTIL_THREAD_FUNC(name)
+#define L4UTIL_THREAD_STATIC_FUNC(name) \
+        L4UTIL_THREAD_FUNC_MIPS_TEMPLATE(name, static)
 
-#define __L4UTIL_THREAD_STATIC_FUNC(name) \
-EXTERN_C_BEGIN \
-void __attribute__((visibility("internal"))) name(void); \
-static void __attribute__((used)) name ##_worker_function(void); \
-asm ("\n" \
-  "  .type " #name ", function \n" \
-  "  .global " #name " \n" \
-  #name ": \n" \
-  "  .set push \n" \
-  "  .set noreorder \n" \
-  L4UTIL_THREAD_START_SETUP_GP \
-  L4UTIL_THREAD_START_LOAD_FUNC_ADDR(#name "_worker_function") \
-  "  jal $t9 \n" \
-  "   nop    \n"\
-  "  .set pop" \
-); \
-EXTERN_C_END \
-static L4_NORETURN void name##_worker_function(void)
-
-#define L4UTIL_THREAD_STATIC_FUNC(name) __L4UTIL_THREAD_STATIC_FUNC(name)
-
+#define L4UTIL_THREAD_FUNC(name) \
+        L4UTIL_THREAD_FUNC_MIPS_TEMPLATE(name, )
 
 #include_next <l4/util/thread.h>
