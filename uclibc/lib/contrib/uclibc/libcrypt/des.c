a5e5933c223c4f74119b00f3468c15162b653508
Max Filippov <jcmvbkbc@gmail.com>
2023-07-24 00:00:00 +0000

    uclibc/des.c: constify des tables
    DES tables take up a huge amount of space in the .bss and they aren't
    even variable. Generate constant tables and put them into des_tables.c
    and drop constants and code used to generate them from des.c
    This saves ~70KBytes of the .bss and ~3KBytes of the total library size:
    
        text           data     bss     dec     hex filename
    - 618508          25652   89400  733560   b3178 lib/libuClibc-1.0.42.so
    + 685664          25672   19488  730824   b26c8 lib/libuClibc-1.0.42.so
    
    Modified libc passes the DES validation suite from the uclibc-ng-test.
    
    Change-Id: I96971782ee3840837e80f85bd0c93cf101d39876
    Signed-off-by: Max Filippov <jcmvbkbc@gmail.com>

--- uclibc/lib/contrib/uclibc/libcrypt/des.c
+++ uclibc/lib/contrib/uclibc/libcrypt/des.c
@@ -64,119 +64,21 @@
 #include <string.h>
 #include <crypt.h>
 #include "libcrypt.h"
+#include "des_tables.c"
 
 /* Re-entrantify me -- all this junk needs to be in
  * struct crypt_data to make this really reentrant... */
-static u_char	inv_key_perm[64];
-static u_char	inv_comp_perm[56];
-static u_char	un_pbox[32];
 static u_int32_t en_keysl[16], en_keysr[16];
 static u_int32_t de_keysl[16], de_keysr[16];
-static u_int32_t ip_maskl[8][256], ip_maskr[8][256];
-static u_int32_t fp_maskl[8][256], fp_maskr[8][256];
-static u_int32_t key_perm_maskl[8][128], key_perm_maskr[8][128];
-static u_int32_t comp_maskl[8][128], comp_maskr[8][128];
 static u_int32_t saltbits;
 static u_int32_t old_salt;
 static u_int32_t old_rawkey0, old_rawkey1;
 
-
-/* Static stuff that stays resident and doesn't change after
- * being initialized, and therefore doesn't need to be made
- * reentrant. */
-static u_char	init_perm[64], final_perm[64];
-static u_char	m_sbox[4][4096];
-static u_int32_t psbox[4][256];
-
-
-
-
 /* A pile of data */
 static const u_char	ascii64[] = "./0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-static const u_char	IP[64] = {
-	58, 50, 42, 34, 26, 18, 10,  2, 60, 52, 44, 36, 28, 20, 12,  4,
-	62, 54, 46, 38, 30, 22, 14,  6, 64, 56, 48, 40, 32, 24, 16,  8,
-	57, 49, 41, 33, 25, 17,  9,  1, 59, 51, 43, 35, 27, 19, 11,  3,
-	61, 53, 45, 37, 29, 21, 13,  5, 63, 55, 47, 39, 31, 23, 15,  7
-};
-
-static const u_char	key_perm[56] = {
-	57, 49, 41, 33, 25, 17,  9,  1, 58, 50, 42, 34, 26, 18,
-	10,  2, 59, 51, 43, 35, 27, 19, 11,  3, 60, 52, 44, 36,
-	63, 55, 47, 39, 31, 23, 15,  7, 62, 54, 46, 38, 30, 22,
-	14,  6, 61, 53, 45, 37, 29, 21, 13,  5, 28, 20, 12,  4
-};
-
 static const u_char	key_shifts[16] = {
 	1, 1, 2, 2, 2, 2, 2, 2, 1, 2, 2, 2, 2, 2, 2, 1
-};
-
-static const u_char	comp_perm[48] = {
-	14, 17, 11, 24,  1,  5,  3, 28, 15,  6, 21, 10,
-	23, 19, 12,  4, 26,  8, 16,  7, 27, 20, 13,  2,
-	41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48,
-	44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32
-};
-
-/*
- *	No E box is used, as it's replaced by some ANDs, shifts, and ORs.
- */
-
-static const u_char	sbox[8][64] = {
-	{
-		14,  4, 13,  1,  2, 15, 11,  8,  3, 10,  6, 12,  5,  9,  0,  7,
-		 0, 15,  7,  4, 14,  2, 13,  1, 10,  6, 12, 11,  9,  5,  3,  8,
-		 4,  1, 14,  8, 13,  6,  2, 11, 15, 12,  9,  7,  3, 10,  5,  0,
-		15, 12,  8,  2,  4,  9,  1,  7,  5, 11,  3, 14, 10,  0,  6, 13
-	},
-	{
-		15,  1,  8, 14,  6, 11,  3,  4,  9,  7,  2, 13, 12,  0,  5, 10,
-		 3, 13,  4,  7, 15,  2,  8, 14, 12,  0,  1, 10,  6,  9, 11,  5,
-		 0, 14,  7, 11, 10,  4, 13,  1,  5,  8, 12,  6,  9,  3,  2, 15,
-		13,  8, 10,  1,  3, 15,  4,  2, 11,  6,  7, 12,  0,  5, 14,  9
-	},
-	{
-		10,  0,  9, 14,  6,  3, 15,  5,  1, 13, 12,  7, 11,  4,  2,  8,
-		13,  7,  0,  9,  3,  4,  6, 10,  2,  8,  5, 14, 12, 11, 15,  1,
-		13,  6,  4,  9,  8, 15,  3,  0, 11,  1,  2, 12,  5, 10, 14,  7,
-		 1, 10, 13,  0,  6,  9,  8,  7,  4, 15, 14,  3, 11,  5,  2, 12
-	},
-	{
-		 7, 13, 14,  3,  0,  6,  9, 10,  1,  2,  8,  5, 11, 12,  4, 15,
-		13,  8, 11,  5,  6, 15,  0,  3,  4,  7,  2, 12,  1, 10, 14,  9,
-		10,  6,  9,  0, 12, 11,  7, 13, 15,  1,  3, 14,  5,  2,  8,  4,
-		 3, 15,  0,  6, 10,  1, 13,  8,  9,  4,  5, 11, 12,  7,  2, 14
-	},
-	{
-		 2, 12,  4,  1,  7, 10, 11,  6,  8,  5,  3, 15, 13,  0, 14,  9,
-		14, 11,  2, 12,  4,  7, 13,  1,  5,  0, 15, 10,  3,  9,  8,  6,
-		 4,  2,  1, 11, 10, 13,  7,  8, 15,  9, 12,  5,  6,  3,  0, 14,
-		11,  8, 12,  7,  1, 14,  2, 13,  6, 15,  0,  9, 10,  4,  5,  3
-	},
-	{
-		12,  1, 10, 15,  9,  2,  6,  8,  0, 13,  3,  4, 14,  7,  5, 11,
-		10, 15,  4,  2,  7, 12,  9,  5,  6,  1, 13, 14,  0, 11,  3,  8,
-		 9, 14, 15,  5,  2,  8, 12,  3,  7,  0,  4, 10,  1, 13, 11,  6,
-		 4,  3,  2, 12,  9,  5, 15, 10, 11, 14,  1,  7,  6,  0,  8, 13
-	},
-	{
-		 4, 11,  2, 14, 15,  0,  8, 13,  3, 12,  9,  7,  5, 10,  6,  1,
-		13,  0, 11,  7,  4,  9,  1, 10, 14,  3,  5, 12,  2, 15,  8,  6,
-		 1,  4, 11, 13, 12,  3,  7, 14, 10, 15,  6,  8,  0,  5,  9,  2,
-		 6, 11, 13,  8,  1,  4, 10,  7,  9,  5,  0, 15, 14,  2,  3, 12
-	},
-	{
-		13,  2,  8,  4,  6, 15, 11,  1, 10,  9,  3, 14,  5,  0, 12,  7,
-		 1, 15, 13,  8, 10,  3,  7,  4, 12,  5,  6, 11,  0, 14,  9,  2,
-		 7, 11,  4,  1,  9, 12, 14,  2,  0,  6, 10, 13, 15,  3,  5,  8,
-		 2,  1, 14,  7,  4, 10,  8, 13, 15, 12,  9,  0,  3,  5,  6, 11
-	}
-};
-
-static const u_char	pbox[32] = {
-	16,  7, 20, 21, 29, 12, 28, 17,  1, 15, 23, 26,  5, 18, 31, 10,
-	 2,  8, 24, 14, 32, 27,  3,  9, 19, 13, 30,  6, 22, 11,  4, 25
 };
 
 static const u_int32_t bits32[32] =
@@ -217,137 +119,15 @@ des_init(void)
 {
 	static int des_initialised = 0;
 
-	int	i, j, b, k, inbit, obit;
-	u_int32_t	*p, *il, *ir, *fl, *fr;
-	const u_int32_t *bits28, *bits24;
-	u_char	u_sbox[8][64];
-
 	if (des_initialised==1)
 		return;
 
 	old_rawkey0 = old_rawkey1 = 0L;
 	saltbits = 0L;
 	old_salt = 0L;
-	bits24 = (bits28 = bits32 + 4) + 4;
-
-	/*
-	 * Invert the S-boxes, reordering the input bits.
-	 */
-	for (i = 0; i < 8; i++)
-		for (j = 0; j < 64; j++) {
-			b = (j & 0x20) | ((j & 1) << 4) | ((j >> 1) & 0xf);
-			u_sbox[i][j] = sbox[i][b];
-		}
-
-	/*
-	 * Convert the inverted S-boxes into 4 arrays of 8 bits.
-	 * Each will handle 12 bits of the S-box input.
-	 */
-	for (b = 0; b < 4; b++)
-		for (i = 0; i < 64; i++)
-			for (j = 0; j < 64; j++)
-				m_sbox[b][(i << 6) | j] =
-					(u_char)((u_sbox[(b << 1)][i] << 4) |
-					u_sbox[(b << 1) + 1][j]);
-
-	/*
-	 * Set up the initial & final permutations into a useful form, and
-	 * initialise the inverted key permutation.
-	 */
-	for (i = 0; i < 64; i++) {
-		init_perm[final_perm[i] = IP[i] - 1] = (u_char)i;
-		inv_key_perm[i] = 255;
-	}
-
-	/*
-	 * Invert the key permutation and initialise the inverted key
-	 * compression permutation.
-	 */
-	for (i = 0; i < 56; i++) {
-		inv_key_perm[key_perm[i] - 1] = (u_char)i;
-		inv_comp_perm[i] = 255;
-	}
-
-	/*
-	 * Invert the key compression permutation.
-	 */
-	for (i = 0; i < 48; i++) {
-		inv_comp_perm[comp_perm[i] - 1] = (u_char)i;
-	}
-
-	/*
-	 * Set up the OR-mask arrays for the initial and final permutations,
-	 * and for the key initial and compression permutations.
-	 */
-	for (k = 0; k < 8; k++) {
-		for (i = 0; i < 256; i++) {
-			*(il = &ip_maskl[k][i]) = 0L;
-			*(ir = &ip_maskr[k][i]) = 0L;
-			*(fl = &fp_maskl[k][i]) = 0L;
-			*(fr = &fp_maskr[k][i]) = 0L;
-			for (j = 0; j < 8; j++) {
-				inbit = 8 * k + j;
-				if (i & bits8[j]) {
-					if ((obit = init_perm[inbit]) < 32)
-						*il |= bits32[obit];
-					else
-						*ir |= bits32[obit-32];
-					if ((obit = final_perm[inbit]) < 32)
-						*fl |= bits32[obit];
-					else
-						*fr |= bits32[obit - 32];
-				}
-			}
-		}
-		for (i = 0; i < 128; i++) {
-			*(il = &key_perm_maskl[k][i]) = 0L;
-			*(ir = &key_perm_maskr[k][i]) = 0L;
-			for (j = 0; j < 7; j++) {
-				inbit = 8 * k + j;
-				if (i & bits8[j + 1]) {
-					if ((obit = inv_key_perm[inbit]) == 255)
-						continue;
-					if (obit < 28)
-						*il |= bits28[obit];
-					else
-						*ir |= bits28[obit - 28];
-				}
-			}
-			*(il = &comp_maskl[k][i]) = 0L;
-			*(ir = &comp_maskr[k][i]) = 0L;
-			for (j = 0; j < 7; j++) {
-				inbit = 7 * k + j;
-				if (i & bits8[j + 1]) {
-					if ((obit=inv_comp_perm[inbit]) == 255)
-						continue;
-					if (obit < 24)
-						*il |= bits24[obit];
-					else
-						*ir |= bits24[obit - 24];
-				}
-			}
-		}
-	}
-
-	/*
-	 * Invert the P-box permutation, and convert into OR-masks for
-	 * handling the output of the S-box arrays setup above.
-	 */
-	for (i = 0; i < 32; i++)
-		un_pbox[pbox[i] - 1] = (u_char)i;
-
-	for (b = 0; b < 4; b++)
-		for (i = 0; i < 256; i++) {
-			*(p = &psbox[b][i]) = 0L;
-			for (j = 0; j < 8; j++) {
-				if (i & bits8[j])
-					*p |= bits32[un_pbox[8 * b + j]];
-			}
-		}
 
 	des_initialised = 1;
 }
-
 
 static void
 setup_salt(u_int32_t salt)
