db6cee978919d27b1226b60b55f6145b14be041b
Toni Dietze <toni.dietze@kernkonzept.com>
2023-07-24 00:00:00 +0000

    L4::Factory::create_gate: update retval
    L4_ENOENT was replaced by L4_EINVAL.
    
    Corresponding source change: Ie912016a39e52e17e4c3c38d6496ed201469ca23
    
    Change-Id: Ie5534cecddb842c640e4c7091dab1a85134f31fa

--- l4sys/include/factory.h
+++ l4sys/include/factory.h
@@ -182,7 +182,7 @@ l4_factory_create_factory_u(l4_cap_idx_t factory, l4_cap_idx_t target_cap,
  *
  * \retval L4_EOK      No error occurred.
  * \retval -L4_ENOMEM  Out-of-memory during allocation of the Ipc_gate object.
- * \retval -L4_ENOENT  `thread_cap` is void or points to something that is not
+ * \retval -L4_EINVAL  `thread_cap` is void or points to something that is not
  *                     a thread.
  * \retval -L4_EPERM   No #L4_CAP_FPAGE_S rights on `factory` or `thread_cap`.
  *
