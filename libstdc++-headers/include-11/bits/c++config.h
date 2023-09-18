fc5e3b370ea629491dbc85492ee55998c1931126
Adam Lackorzynski <adam@l4re.org>
2023-09-18 00:00:00 +0000

    libstdc++-v3: Update gcc-11 variant
    Taken from b3bcb1301a3c8ba8f0b46c6c79c1c9d9510e9281
    
    Change-Id: I5be57322b022bbc607ecaa4e2908b6769c51dd30

--- libstdc++-headers/include-11/bits/c++config.h
+++ libstdc++-headers/include-11/bits/c++config.h
@@ -34,7 +34,7 @@
 #define _GLIBCXX_RELEASE 11
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20230618
+#define __GLIBCXX__ 20230917
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
