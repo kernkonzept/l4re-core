306a06de069fc54bdd848e85cf76695bad277e29
Adam Lackorzynski <adam@l4re.org>
2023-12-04 00:00:00 +0000

    libstdc++-v3: Update gcc-10 variant
    Taken from d04fe5541c53cb16d1ca5c80da044b4c7633dbc6
    
    Change-Id: I7bd48fe525283c705c0bda68262a75b44212e2d3

--- libstdc++-headers/include-10/bits/c++config.h
+++ libstdc++-headers/include-10/bits/c++config.h
@@ -34,7 +34,7 @@
 #define _GLIBCXX_RELEASE 10
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20230604
+#define __GLIBCXX__ 20230707
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
