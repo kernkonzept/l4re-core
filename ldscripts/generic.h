682757638c35d5cf89e056a0e5683ab23116d4ae
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2024-03-18 00:00:00 +0000

    ldscripts: remove unused definition
    Change-Id: I1907c5cfa4f7f52bc88ab88ec172dbdc1be6e125

--- ldscripts/generic.h
+++ ldscripts/generic.h
@@ -3,11 +3,6 @@
 #define L4_DEFINE_L4PHDRS \
    l4re_aux   0x60000014;
 
-#define L4_DEFINE_L4PHDRS___DISABLED \
-   stack 0x60000012 AT (__L4_STACK_ADDR__); \
-   kip   0x60000013 AT (__L4_KIP_ADDR__); \
-   l4re_aux   0x60000014;
-
 #define L4_DEFINE_X86_KERNEL_ENTRY_SYMS \
    PROVIDE(__l4sys_invoke_direct = 0xeacff000 + 0x000);
 
