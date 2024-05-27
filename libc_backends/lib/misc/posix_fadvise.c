c92cb4a91040d0816d806b1d7f09be05ad914b2f
Adam Lackorzynski <adam@l4re.org>
2024-05-27 00:00:00 +0000

    libc_be_misc: Add posix_fadvise stub
    Do the same as for posix_fadvise64.
    
    Change-Id: Id51301413c1c380588aefd74c7b9796d4a7c423b

--- libc_backends/lib/misc/posix_fadvise.c
+++ libc_backends/lib/misc/posix_fadvise.c
@@ -14,3 +14,10 @@ int posix_fadvise64(int fd, off64_t offset, off64_t len, int advice)
          fd, (unsigned long long)offset, (unsigned long long)len, advice);
   return 0;
 }
+
+int posix_fadvise(int fd, off_t offset, off_t len, int advice)
+{
+  printf("posix_fadvise(%d, %lld, %lld, %d): void\n",
+         fd, (unsigned long long)offset, (unsigned long long)len, advice);
+  return 0;
+}
