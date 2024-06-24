6aab7dc93d37e79db564325b11241b4a3ef033f6
Adam Lackorzynski <adam@l4re.org>
2024-06-24 00:00:00 +0000

    libc/pthread: Add pthread_sigmask
    Change-Id: I469a409482d9fb4cf734c002441ba80258baf6d1

--- uclibc/lib/libpthread/src/signals.c
+++ uclibc/lib/libpthread/src/signals.c
@@ -4,6 +4,52 @@
 #include <errno.h>
 #include <pthread-l4.h>
 #include <l4/sys/thread.h>
+#include <bits/sigthread.h>
+#include "internals.h"
+
+// In case no backend is available
+__attribute__((weak))
+int sigprocmask(int how, const sigset_t *set, sigset_t *oldset)
+{
+  (void)how;
+  (void)set;
+  (void)oldset;
+  errno = ENOSYS;
+  return -1;
+}
+
+
+int pthread_sigmask(int how, const sigset_t * newmask, sigset_t * oldmask)
+{
+  sigset_t mask;
+
+  if (newmask != NULL) {
+    mask = *newmask;
+    /* Don't allow __pthread_sig_restart to be unmasked.
+       Don't allow __pthread_sig_cancel to be masked. */
+    switch(how) {
+    case SIG_SETMASK:
+      sigaddset(&mask, __pthread_sig_restart);
+      sigdelset(&mask, __pthread_sig_cancel);
+      if (__pthread_sig_debug > 0)
+	sigdelset(&mask, __pthread_sig_debug);
+      break;
+    case SIG_BLOCK:
+      sigdelset(&mask, __pthread_sig_cancel);
+      if (__pthread_sig_debug > 0)
+	sigdelset(&mask, __pthread_sig_debug);
+      break;
+    case SIG_UNBLOCK:
+      sigdelset(&mask, __pthread_sig_restart);
+      break;
+    }
+    newmask = &mask;
+  }
+  if (sigprocmask(how, newmask, oldmask) == -1)
+    return errno;
+  else
+    return 0;
+}
 
 int pthread_kill(pthread_t thread, int signo);
 int pthread_kill(pthread_t thread, int signo)
--- uclibc/lib/uclibc/ARCH-all/include/bits/sigthread.h
+++ uclibc/lib/uclibc/ARCH-all/include/bits/sigthread.h
@@ -8,5 +8,7 @@
 # error "Never include this file directly.  Use <pthread.h> instead"
 #endif
 
+extern int pthread_sigmask(int how, const sigset_t * newmask, sigset_t * oldmask);
+
 /* Send signal SIGNO to the given thread. */
 extern int pthread_kill (pthread_t __threadid, int __signo) __THROW;
