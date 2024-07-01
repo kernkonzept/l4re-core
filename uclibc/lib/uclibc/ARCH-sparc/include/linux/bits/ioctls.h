802909bfd11dba15763e620ba4958d05b2c48fd2
Marcus Haehnel <marcus.haehnel@kernkonzept.com>
2024-07-01 00:00:00 +0000

    uclibc: Replace FSF snail mail address with URLs
    This matches similar glibc and uclibc-ng commits for our own copies of
    files.
    
    Change-Id: Ibd01e9cbc5ca6c5167c6972572b19d3f356b826f

--- uclibc/lib/uclibc/ARCH-sparc/include/linux/bits/ioctls.h
+++ uclibc/lib/uclibc/ARCH-sparc/include/linux/bits/ioctls.h
@@ -1,5 +1,4 @@
 /* Copyright (C) 1996, 1997 Free Software Foundation, Inc.
-   This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
@@ -12,9 +11,8 @@
    Lesser General Public License for more details.
 
    You should have received a copy of the GNU Lesser General Public
-   License along with the GNU C Library; if not, write to the Free
-   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
-   02111-1307 USA.  */
+   License along with the GNU C Library; if not, see
+   <http://www.gnu.org/licenses/>.  */
 
 #ifndef _SYS_IOCTL_H
 # error "Never use <bits/ioctls.h> directly; include <sys/ioctl.h> instead."
