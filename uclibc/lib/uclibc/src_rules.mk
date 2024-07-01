99b93ee0e20c219810ac62fc4f7e917eb0d9596b
Marcus Haehnel <marcus.haehnel@kernkonzept.com>
2024-07-01 00:00:00 +0000

    uclibc/buildsys: temporary fix to symlinks in builddir
    Change Ide791319cbe95e24bbdd1da252eae3049d61b84c introduced a symlink
    that is later replaced with a real directory. cp -srf does not real well
    with this case and refuses to overwrite the symlink. Clean the
    destination directory before symlinking to avoid this case.
    
    Since the change was only life for a day remove the workaround again
    after a week.
    
    Change-Id: Ia3f9a651b884322baf64e0a1f0b09e6fed99c4a9

--- uclibc/lib/uclibc/src_rules.mk
+++ uclibc/lib/uclibc/src_rules.mk
@@ -14,6 +14,7 @@ LIBC_DST_DIR  := $(OBJ_DIR)/src
 $(LIBC_DST_DIR)/.links-done: $(SRC_DIR)/Makefile $(LIBCSRC_DIR)/src_rules.mk \
                              $(LIBCSRC_DIR)/contrib_files.mk $(LIBCSRC_DIR)/make_vars.mk \
                              $(shell find $(LIBC_SRC_DIRS) -type f)
+	$(VERBOSE)$(RM) -r $(LIBC_DST_DIR)
 	$(VERBOSE)$(MKDIR) $(LIBC_DST_DIR)
 	$(VERBOSE)$(CP) -sfr $(LIBC_SRC_DIRS) $(LIBC_DST_DIR)
 	$(VERBOSE)touch $@
