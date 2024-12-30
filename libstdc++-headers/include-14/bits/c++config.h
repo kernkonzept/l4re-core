dc2f53cad7334c678decf69da36d0139a1a23bcd
Adam Lackorzynski <adam@l4re.org>
2024-12-30 00:00:00 +0000

    libstdc++-v3: Update gcc-14 variant
    Taken from 656425e162dec607ad4a03cb4babe42f57d00a8c
    
    Change-Id: I45b7d8e0a1f5e5b8b95b9429ff9c0962ec2846d2

--- libstdc++-headers/include-14/bits/c++config.h
+++ libstdc++-headers/include-14/bits/c++config.h
@@ -36,7 +36,7 @@
 #define _GLIBCXX_RELEASE 14
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20241124
+#define __GLIBCXX__ 20241229
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
