a491adda36a95a6c5265a8cf75b8a36f56572468
Adam Lackorzynski <adam@l4re.org>
2024-12-30 00:00:00 +0000

    libstdc++-v3: Update gcc-13 variant
    Taken from 822639b8b5022b5428018792e9ff7a823473eaa7
    
    Change-Id: If3adb6450ea2d34d839456189d8e66fe388957db

--- libstdc++-headers/include-13/bits/c++config.h
+++ libstdc++-headers/include-13/bits/c++config.h
@@ -34,7 +34,7 @@
 #define _GLIBCXX_RELEASE 13
 
 // The datestamp of the C++ library in compressed ISO date format.
-#define __GLIBCXX__ 20241124
+#define __GLIBCXX__ 20241215
 
 // Macros for various attributes.
 //   _GLIBCXX_PURE
