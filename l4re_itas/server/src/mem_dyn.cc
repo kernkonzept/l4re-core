86425d7c5785fdb7101b39ece82e441d43d09604
Martin Decky <martin.decky@kernkonzept.com>
2025-09-15 00:00:00 +0000

    Avoid unintentional and document intentional capability leak
    Make sure the dataspace capability is not leaked unintentionally in
    umalloc_area_create() by using Unique_cap. Explicitly document why
    we allow the capability to be leaked intentionally at the end of the
    function.
    
    Suggested-by: Philipp Eppelt <philipp.eppelt@kernkonzept.com>
    Change-Id: Ib2f1bc03ddfa2784a668529204cdec904303ed33

--- l4re_itas/server/src/mem_dyn.cc
+++ l4re_itas/server/src/mem_dyn.cc
@@ -12,6 +12,7 @@
 #include <l4/re/dataspace>
 #include <l4/re/mem_alloc>
 #include <l4/re/error_helper>
+#include <l4/re//unique_cap>
 #include <l4/cxx/iostream>
 #include <l4/libumalloc/umalloc.h>
 #include "globals.h"
@@ -40,12 +41,12 @@ static void *err_msg(char const *msg, size_t area_size, long err = 0)
 void *umalloc_area_create(size_t area_size) noexcept
 {
   // Allocate a dataspace.
-  L4::Cap<L4Re::Dataspace> ds = Global::cap_alloc->alloc<L4Re::Dataspace>();
-  if (!ds)
+  auto ds = L4Re::make_unique_cap<L4Re::Dataspace>(Global::cap_alloc);
+  if (!ds.is_valid())
     return err_msg("Failed to allocate dataspace capability", area_size);
 
   // Allocate physical memory.
-  long err = Global::allocator->alloc(area_size, ds);
+  auto err = Global::allocator->alloc(area_size, ds.get());
   if (err < 0)
     return err_msg("Failed to allocate memory", area_size, err);
 
@@ -60,20 +61,25 @@ void *umalloc_area_create(size_t area_size) noexcept
 
   // Attach the memory.
   err = L4Re::Env::env()->rm()->attach(&start, area_size, flags,
-                                       L4::Ipc::make_cap_rw(ds),
+                                       L4::Ipc::make_cap_rw(ds.get()),
                                        0, L4_PAGESHIFT,
                                        L4::Cap<L4::Task>::Invalid,
                                        "[itas-heap]");
   if (err < 0)
-    {
-      Global::cap_alloc->free(ds);
-      return err_msg("Failed to attach memory", area_size, err);
-    }
+    return err_msg("Failed to attach memory", area_size, err);
 
 #if defined(CONFIG_MMU)
   // Note that the starting address is guaranteed to be page-aligned.
   umalloc_pos = start + l4_round_page(area_size);
 #endif
+
+  // The current implementation of libumalloc does not dispose of unused/empty
+  // heap areas. When this is implemented eventually, we will have to retain
+  // the dataspace capability for the possible future disposal.
+  //
+  // So far, we just explicitly leak the dataspace capability here. The
+  // capability will be automatically disposed during program termination.
+  ds.release();
 
   return reinterpret_cast<void *>(start);
 }
