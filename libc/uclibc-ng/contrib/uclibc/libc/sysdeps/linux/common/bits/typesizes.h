5be02a7c7e2c732552a10a1eb01a6a4cd1fe37ff
Waldemar Brodkorb <wbx@openadk.org>
2025-09-15 00:00:00 +0000

    uclibc-ng: Add glibc internal macros for 64 Bit architectures
    To satisfy detection of LFS support add these glibc internal macros.
    Fixes f.e. libselinux compile.
    
    Change-Id: I65b893c89e79b1fb906cbd61273a0cac63d2cc48
    Reported-By: Thomas Petazzoni <thomas.petazzoni@bootlin.com>
    Signed-off-by: Marcus Haehnel <marcus.haehnel@kernkonzept.com>

--- libc/uclibc-ng/contrib/uclibc/libc/sysdeps/linux/common/bits/typesizes.h
+++ libc/uclibc-ng/contrib/uclibc/libc/sysdeps/linux/common/bits/typesizes.h
@@ -73,5 +73,15 @@
 /* Number of descriptors that can fit in an `fd_set'.  */
 #define	__FD_SETSIZE		1024
 
+#if __WORDSIZE == 64
+/* Tell the code that off_t and off64_t are actually the same type
+   for all ABI purposes, even if possibly expressed as different base types
+   for C type-checking purposes.  */
+# define __OFF_T_MATCHES_OFF64_T	1
+
+/* Same for ino_t and ino64_t.  */
+# define __INO_T_MATCHES_INO64_T	1
+
+#endif
 
 #endif /* bits/typesizes.h */
