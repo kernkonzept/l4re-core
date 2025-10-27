31121d201dcefee9467f79e994a21b93e0575cb1
Georg Kotheimer <georg.kotheimer@kernkonzept.com>
2025-10-27 00:00:00 +0000

    l4sys: Replace magic value in l4_invoke_debugger with constant
    To make the purpose of the addition to mr[0] more obvious we use the
    proper constant L4_KDEBUG_GROUP_KOBJ instead of a magic value.
    
    Co-authored-by: Jan Klötzke <jan.kloetzke@kernkonzept.com>
    Change-Id: I60b4ff94f4ddcf1548cf6bd98515e4b08af8e47e

--- l4sys/include/__kernel_object_impl.h
+++ l4sys/include/__kernel_object_impl.h
@@ -6,6 +6,7 @@
 #pragma once
 
 #include <l4/sys/ipc.h>
+#include <l4/sys/kdebug.h>
 
 L4_INLINE l4_msgtag_t
 l4_invoke_debugger(l4_cap_idx_t obj, l4_msgtag_t tag, l4_utcb_t *utcb) L4_NOTHROW
@@ -20,7 +21,7 @@ l4_invoke_debugger(l4_cap_idx_t obj, l4_msgtag_t tag, l4_utcb_t *utcb) L4_NOTHRO
   if (words + 2 > L4_UTCB_GENERIC_DATA_SIZE)
     return l4_msgtag(-L4_EMSGTOOLONG, 0, 0, 0);
 
-  mr->mr[0] += 0x100;
+  mr->mr[0] += L4_KDEBUG_GROUP_KOBJ;
   mr->mr[words]     = L4_ITEM_MAP;
   mr->mr[words + 1] = l4_obj_fpage(obj, 0, L4_CAP_FPAGE_RWS).raw;
   t2 = l4_msgtag(L4_PROTO_DEBUGGER, words, 1, l4_msgtag_flags(tag));
