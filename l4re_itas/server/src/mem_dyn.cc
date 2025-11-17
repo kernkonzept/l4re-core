cc2c56910edb43a6437bdc858d2674ceede4d781
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2025-11-17 00:00:00 +0000

    l4re_itas: use anonymous region mapping instead of dataspace
    This saves us a capability. More importantly, it will provide proper
    fork() semantics later.
    
    Change-Id: I15e0355512e62280122966c537369aa72a415746
    Signed-off-by: Jan Klötzke <jan.kloetzke@kernkonzept.com>

--- l4re_itas/server/src/mem_dyn.cc
+++ l4re_itas/server/src/mem_dyn.cc
@@ -46,17 +46,8 @@ static void *err_msg(char const *msg, size_t area_size, long err = 0)
 
 void *umalloc_area_create(size_t area_size) noexcept
 {
-  // Allocate a dataspace.
-  auto ds = L4Re::make_unique_cap<L4Re::Dataspace>(Global::cap_alloc);
-  if (!ds.is_valid())
-    return err_msg("Failed to allocate dataspace capability", area_size);
-
-  // Allocate physical memory.
-  auto err = Global::allocator->alloc(area_size, ds.get());
-  if (err < 0)
-    return err_msg("Failed to allocate memory", area_size, err);
-
-  L4Re::Rm::Flags flags(L4Re::Rm::F::RW);
+  L4Re::Rm::Flags flags(L4Re::Rm::F::RW | L4Re::Rm::F::Private
+                        | L4Re::Rm::F::Anonymous);
 
 #if defined(CONFIG_MMU)
   // Make sure we did not exhaust our reserved virtual memory area. We cannot
@@ -75,11 +66,11 @@ void *umalloc_area_create(size_t area_size) noexcept
 #endif
 
   // Attach the memory.
-  err = L4Re::Env::env()->rm()->attach(&start, area_size, flags,
-                                       L4::Ipc::make_cap_rw(ds.get()),
-                                       0, L4_PAGESHIFT,
-                                       L4::Cap<L4::Task>::Invalid,
-                                       "[itas-heap]");
+  l4_ret_t err = L4Re::Env::env()->rm()->attach(&start, area_size, flags,
+                                                L4::Cap<L4Re::Dataspace>(),
+                                                0, L4_PAGESHIFT,
+                                                L4::Cap<L4::Task>::Invalid,
+                                                "[itas-heap]");
   if (err < 0)
     return err_msg("Failed to attach memory", area_size, err);
 
@@ -87,14 +78,6 @@ void *umalloc_area_create(size_t area_size) noexcept
   // Note that the starting address is guaranteed to be page-aligned.
   umalloc_pos = start + l4_round_page(area_size);
 #endif
-
-  // The current implementation of libumalloc does not dispose of unused/empty
-  // heap areas. When this is implemented eventually, we will have to retain
-  // the dataspace capability for the possible future disposal.
-  //
-  // So far, we just explicitly leak the dataspace capability here. The
-  // capability will be automatically disposed during program termination.
-  ds.release();
 
   return reinterpret_cast<void *>(start);
 }
