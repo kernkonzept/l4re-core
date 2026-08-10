18e844e2c25f7389f2655993f942e1c87203101c
Georg Kotheimer <georg.kotheimer@kernkonzept.com>
2026-08-10 00:00:00 +0000

    l4re: Add memory type argument to Mem_alloc::alloc()
    See ticket #CD-1820.
    
    Change-Id: I698832bb8187ebb27d080a1435d3e7b32d53b886
    Signed-off-by: Georg Kotheimer <georg.kotheimer@kernkonzept.com>

--- l4re/include/impl/mem_alloc_impl.h
+++ l4re/include/impl/mem_alloc_impl.h
@@ -20,9 +20,9 @@ namespace L4Re
 {
 
 l4_ret_t
-Mem_alloc::alloc(long size,
-                 L4::Cap<Dataspace> mem, unsigned long flags,
-                 unsigned long align, l4_addr_t paddr) const noexcept
+Mem_alloc::alloc(long size, L4::Cap<Dataspace> mem, unsigned long flags,
+                 unsigned long align, l4_addr_t paddr,
+                 char const *type) const noexcept
 {
   L4::Cap<L4::Factory> f(cap());
   auto call = f->create(mem, L4Re::Dataspace::Protocol);
@@ -31,6 +31,21 @@ Mem_alloc::alloc(long size,
        << l4_umword_t(align);
   if (flags & Fixed_paddr)
     call << l4_umword_t(paddr);
+  if (type)
+    {
+      // We have to prepend 'type='.
+      static constexpr unsigned Prefix_len = 5;
+      char type_buf[32]; // assuming that type length is limited
+      auto len = __builtin_strlen(type);
+      if (len > sizeof(type_buf) - Prefix_len - 1)
+        return -L4_EINVAL; // name too long
+      __builtin_memcpy(type_buf, "type=", Prefix_len);
+      __builtin_memcpy(&type_buf[Prefix_len], type, len);
+      type_buf[Prefix_len + len] = '\0';
+
+      call << type_buf;
+    }
+
   return l4_error(call);
 }
 
