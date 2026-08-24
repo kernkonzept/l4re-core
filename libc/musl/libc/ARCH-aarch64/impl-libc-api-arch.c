fdb30c331f12cbb1883fb4e1500c389284b2cb86
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2026-08-24 00:00:00 +0000

    libc/musl: fix arm64 ptlc_set_tp()
    The function computed tp = tls_tp + TP_OFFSET but wrote tls_tp into
    TPIDR_EL0. Fortunately TP_OFFSET is 0 on arm64. Still, it generated an
    unused variable warning and was inconsistent with the other
    architectures.
    
    Change-Id: I9a4107ce50f12edcabdd1b91c413f08c30255274
    Signed-off-by: Jan Klötzke <jan.kloetzke@kernkonzept.com>

--- libc/musl/libc/ARCH-aarch64/impl-libc-api-arch.c
+++ libc/musl/libc/ARCH-aarch64/impl-libc-api-arch.c
@@ -4,6 +4,6 @@ int
 ptlc_set_tp(void *tls_tp)
 {
   void *tp = (char *)tls_tp + TP_OFFSET;
-  __asm __volatile("msr tpidr_el0, %0" : : "r"(tls_tp));
+  __asm __volatile("msr tpidr_el0, %0" : : "r"(tp));
   return 0;
 }
