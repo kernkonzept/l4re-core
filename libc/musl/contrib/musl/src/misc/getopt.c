f798241c8b55666cf95a81487283f52d32e4ebb4
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2026-08-24 00:00:00 +0000

    libc/musl: fix warning about unused value
    The __getopt_msg function uses the shortcut property of && to express a
    nested if. Unfortunately the compiler warns about the unused computed
    boolean value. Explicitly cast it to void to suppress the warning.
    
    Change-Id: I1226682332df4f11bbbe27c7b1525729efa5a324
    Signed-off-by: Jan Klötzke <jan.kloetzke@kernkonzept.com>

--- libc/musl/contrib/musl/src/misc/getopt.c
+++ libc/musl/contrib/musl/src/misc/getopt.c
@@ -18,10 +18,10 @@ void __getopt_msg(const char *a, const char *b, const char *c, size_t l)
 	FILE *f = stderr;
 	b = __lctrans_cur(b);
 	FLOCK(f);
-	fputs(a, f)>=0
+	(void)(fputs(a, f)>=0
 	&& fwrite(b, strlen(b), 1, f)
 	&& fwrite(c, 1, l, f)==l
-	&& putc('\n', f);
+	&& putc('\n', f));
 	FUNLOCK(f);
 }
 
