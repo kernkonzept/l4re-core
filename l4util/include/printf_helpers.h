74de9620f7ccb1d38e2f2cd6c03562e2011fdd85
Frank Mehnert <frank.mehnert@kernkonzept.com>
2026-08-31 00:00:00 +0000

    l4util/printf_helpers: add l4util_human_readable_size_region()
    Printing the size of region 0...~0ULL is not possible with
    l4util_human_readable_size(): The passed value had to be ~0ULL + 1 = 0.
    
    Introduce l4util_human_readable_size_region() taking the start and end
    of the region as parameters and printing the correct string for the
    special case.
    
    Change-Id: I3f18b74624fc675e29eae77e821aa6d3e51f8f98
    Signed-off-by: Frank Mehnert <frank.mehnert@kernkonzept.com>

--- l4util/include/printf_helpers.h
+++ l4util/include/printf_helpers.h
@@ -50,3 +50,35 @@ L4_INLINE int l4util_human_readable_size(char *outstr, size_t outsize,
   else
     return snprintf(outstr, outsize, "%llu B  ", value);
 }
+
+/**
+ * Print the human-readable size of a given given region begin..end.
+ *
+ * This function does the same as l4util_human_readable_size() but it properly
+ * handles the size of a region spawning the whole address space 0..~0UL.
+ *
+ * The generated output has the format
+ * \code
+ * i.f <unit>
+ * \endcode
+ * with
+ * - 'i' being the integer part of 'bytes' with 1-3 digits,
+ * - 'f' being the fractional part of 'bytes' with always 1 digit,
+ * - 'unit' being the unit of 'v.f', for instance "123.4 MiB" or "4.5 GiB".
+ * Values < 1024 are printed as 'v B', for instance "1023 B".
+ *
+ * \param outstr   The string to print the size.
+ * \param outsize  The size of the string to print.
+ * \param begin    The first byte of the region.
+ * \param end      The last byte (inclusive) of the region.
+ */
+L4_INLINE int l4util_human_readable_size_region(char *outstr, size_t outsize,
+                                                unsigned long long begin,
+                                                unsigned long long end)
+{
+  if (end - begin == ~0UL)
+    return snprintf(outstr, outsize, "%s",
+                    sizeof(long) == 4 ? "4.0 GiB" : "16.0 EiB");
+  else
+    return l4util_human_readable_size(outstr, outsize, end - begin);
+}
