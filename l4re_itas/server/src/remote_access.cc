6f60c9246c2f2918d79c842308d342011a03e691
Frank Mehnert <frank.mehnert@kernkonzept.com>
2026-08-31 00:00:00 +0000

    l4re_itas/remote_access: bound the retry loop in read_mem()
    Since I5b490e4d1ddd6ffd866d68922f49e876f91e1b33, pagein() is reached
    only after an access already faulted. Retry the access once after paging
    in and report -L4_EFAULT if it still faults.
    
    The last_pfn handling is no longer necessary.
    
    Change-Id: Ic9b534beb09c53baa53cda64c764496f824cbc1e
    Signed-off-by: Frank Mehnert <frank.mehnert@kernkonzept.com>

--- l4re_itas/server/src/remote_access.cc
+++ l4re_itas/server/src/remote_access.cc
@@ -13,69 +13,57 @@
 
 Remote_access ra_if;
 
-static l4_addr_t last_pfn = ~0ul;
-
 static l4_ret_t pagein(l4_addr_t addr, bool with_write, bool with_exec)
 {
-  if (l4_trunc_page(addr) != last_pfn)
-    {
-      // Get read-only access to region map. Needs to be stored in variable to
-      // retain read-lock until end of scope!
-      auto rm = Global::local_rm->read_access();
+  // Get read-only access to region map. Needs to be stored in variable to
+  // retain read-lock until end of scope!
+  auto rm = Global::local_rm->read_access();
 
-      Region_map::Node node = rm->find(addr);
-      if (!node)
-        return -L4_ENOENT;
+  Region_map::Node node = rm->find(addr);
+  if (!node)
+    return -L4_ENOENT;
 
-      if (!(  node->second.flags()
-            & (L4Re::Rm::F::Kernel | L4Re::Rm::F::Reserved)))
-        {
-          addr &= ~7ul;
-          addr |= with_write ? 2 : 0;
-          addr |= with_exec  ? 4 : 0;
-          L4::Ipc::Opt<L4::Ipc::Snd_fpage> fp;
-          // We could split op_page_fault function to avoid the find in
-          // there which we just did.
-          l4_ret_t ret = Global::local_rm->op_page_fault(0, addr, 0, fp);
-          if (ret)
-            return ret;
-        }
+  if (node->second.flags() & (L4Re::Rm::F::Kernel | L4Re::Rm::F::Reserved))
+    return 0;
 
-      last_pfn = l4_trunc_page(addr);
-    }
-  return 0;
+  addr &= ~7ul;
+  addr |= with_write ? 2 : 0;
+  addr |= with_exec  ? 4 : 0;
+  L4::Ipc::Opt<L4::Ipc::Snd_fpage> fp;
+  // We could split op_page_fault to avoid the find in there which we just did.
+  return Global::local_rm->op_page_fault(0, addr, 0, fp);
 }
 
 l4_ret_t Remote_access::op_read_mem(L4Re::Remote_access::Rights,
                                     l4_addr_t addr, char width, l4_uint64_t &val)
 {
-  bool smr;
-
-  do
+  for (bool paged_in = false;; paged_in = true)
     {
+      bool smr;
+
       switch (width)
         {
         case L4Re::Remote_access::Wd_8bit:
-            {
-              l4_uint8_t v = 0;
-              smr = safe_memcpy(&v, reinterpret_cast<char const *>(addr), 1);
-              val = v;
-            }
-          break;
+          {
+            l4_uint8_t v = 0;
+            smr = safe_memcpy(&v, reinterpret_cast<char const *>(addr), 1);
+            val = v;
+            break;
+          }
         case L4Re::Remote_access::Wd_16bit:
-            {
-              l4_uint16_t v = 0;
-              smr = safe_memcpy(&v, reinterpret_cast<char const *>(addr), 2);
-              val = v;
-            }
-          break;
+          {
+            l4_uint16_t v = 0;
+            smr = safe_memcpy(&v, reinterpret_cast<char const *>(addr), 2);
+            val = v;
+            break;
+          }
         case L4Re::Remote_access::Wd_32bit:
-            {
-              l4_uint32_t v = 0;
-              smr = safe_memcpy(&v, reinterpret_cast<char const *>(addr), 4);
-              val = v;
-            }
-          break;
+          {
+            l4_uint32_t v = 0;
+            smr = safe_memcpy(&v, reinterpret_cast<char const *>(addr), 4);
+            val = v;
+            break;
+          }
         case L4Re::Remote_access::Wd_64bit:
           smr = safe_memcpy(&val, reinterpret_cast<char const *>(addr), 8);
           break;
@@ -83,13 +71,15 @@ l4_ret_t Remote_access::op_read_mem(L4Re::Remote_access::Rights,
           return -L4_EINVAL;
         };
 
-      if (!smr)
-        if (l4_ret_t r = pagein(addr, 0, 0))
-          return r;
-    }
-  while (!smr);
+      if (smr)
+        return 0;
 
-  return 0;
+      if (paged_in)
+        return -L4_EFAULT;
+
+      if (l4_ret_t r = pagein(addr, 0, 0))
+        return r;
+    }
 }
 
 l4_ret_t Remote_access::op_write_mem(L4Re::Remote_access::Rights,
