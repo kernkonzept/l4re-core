51887416c3e76963a52c3eb45ba9d04e417b9e96
Frank Mehnert <frank.mehnert@kernkonzept.com>
2026-08-31 00:00:00 +0000

    libc_backends: return a well-defined cpu set from sched_getaffinity()
    Fix the CPU bit number: Use 0 instead of 1.
    And clear the bitmask before setting any bit.
    
    Change-Id: I2b534e2c24593a02dc4016966469e2e18fe1cf5b
    Signed-off-by: Frank Mehnert <frank.mehnert@kernkonzept.com>

--- libc_backends/lib/misc/sched.c
+++ libc_backends/lib/misc/sched.c
@@ -30,7 +30,8 @@ int sched_getaffinity(pid_t pid, size_t cpusetsize,
                       cpu_set_t *mask)
 {
   (void)pid;
-  CPU_SET_S(1, cpusetsize, mask);
+  CPU_ZERO_S(cpusetsize, mask);
+  CPU_SET_S(0, cpusetsize, mask);
   return 0;
 }
 
