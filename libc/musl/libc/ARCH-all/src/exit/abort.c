72b29be9aa35c5df90f25add11f714c4364ead9b
Jan Klötzke <jan.kloetzke@kernkonzept.com>
2026-09-14 00:00:00 +0000

    libc/musl: implement abort() correctly
    Calling abort() must raise SIGABRT. An application may have installed a
    handler to act upon that.
    
    At the same time, the abort() function must ensure that the process
    terminates. So in case the signal was blocked, ignored or the handler
    returned, we need to try again. Upstream glibc and musl prevent
    modifications of the signal disposition by grabbing a lock. We choose
    to simply retry until the signal was neither blocked nor changed. That
    is slightly less robust but fully backed by the POSIX standard.
    
    Also adjust the exit code in case the signal does not work to match
    glibc and musl.
    
    Change-Id: If23f3ab6ae2f2bcdbee0112f82be50e1aeb1ab0c
    Signed-off-by: Jan Klötzke <jan.kloetzke@kernkonzept.com>

--- libc/musl/libc/ARCH-all/src/exit/abort.c
+++ libc/musl/libc/ARCH-all/src/exit/abort.c
@@ -1,7 +1,42 @@
+#include <signal.h>
 #include <stdlib.h>
+#include <string.h>
+
+#include "atomic.h"
 
 _Noreturn void abort(void)
 {
-  _Exit(128);
-}
+#ifndef L4_MINIMAL_LIBC
+  sigset_t sigs, oldsigs;
+  struct sigaction act, oldact;
 
+  do
+    {
+      raise(SIGABRT);
+
+      // We've survived SIGABRT. Either the signal was blocked, ignored or the
+      // handler returned. Unblock SIGABRT and restore default disposition.
+      // Because another thread could compete, do it in the loop until no
+      // signal handler was registered.
+
+      sigemptyset(&sigs);
+      sigaddset(&sigs, SIGABRT);
+      if (sigprocmask(SIG_UNBLOCK, &sigs, &oldsigs) < 0)
+        break;
+
+      memset(&act, 0, sizeof(act));
+      act.sa_handler = SIG_DFL;
+      sigfillset(&act.sa_mask);
+      if (sigaction(SIGABRT, &act, &oldact) < 0)
+        break;
+    }
+  while (sigismember(&oldsigs, SIGABRT) || oldact.sa_handler != SIG_DFL);
+
+  // Effectively unreachable unless another thread flips the SIGABRT
+  // disposition repeatedly. Try to raise an exception...
+  a_crash();
+#endif
+
+  // Still around? Tell the parent and hope that it acts on that...
+  _Exit(127);
+}
